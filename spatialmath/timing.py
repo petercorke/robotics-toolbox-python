12070cc9aba6bf8e6937d27c08cd3058f8020373
Peter Corke <peter.i.corke@gmail.com>
2020-04-12 17:23:13 +1000

    make safe to import, needed for sphinx docgen

--- spatialmath/timing.py
+++ spatialmath/timing.py
@@ -7,33 +7,35 @@ Created on Fri Apr 10 14:22:36 2020
 """
 
 
-import timeit
+if __name__ == '__main__':
 
-N = 1000000
+    import timeit
 
-quat_setup = '''import spatialmath.base.quaternion as quat
-import spatialmath.quaternion as qq
-import numpy as np
-q1 = quat.qrand()
-q2 = quat.qrand()
-v = np.r_[1,2,3]
-Q1 = qq.UnitQuaternion.Rx(0.2)
-Q2 = qq.UnitQuaternion.Ry(0.3)'''
+    N = 1000000
 
-transforms_setup = '''import spatialmath.base.transforms as tr'''
+    quat_setup = '''import spatialmath.base.quaternion as quat
+    import spatialmath.quaternion as qq
+    import numpy as np
+    q1 = quat.qrand()
+    q2 = quat.qrand()
+    v = np.r_[1,2,3]
+    Q1 = qq.UnitQuaternion.Rx(0.2)
+    Q2 = qq.UnitQuaternion.Ry(0.3)'''
 
-
-t = timeit.timeit(stmt='tr.rotx(0.2)', setup=transforms_setup, number=N)
-print('transforms.rotx: ', t, ' us')
+    transforms_setup = '''import spatialmath.base.transforms as tr'''
 
 
-t = timeit.timeit(stmt='a = quat.qqmul(q1,q2)', setup=quat_setup, number=N)
-print('quat.qqmul:         ', t, ' us')
-t = timeit.timeit(stmt='a = quat.qvmul(q1,v)', setup=quat_setup, number=N)
-print('quat.qqmul:         ', t, ' us')
-t = timeit.timeit(stmt='a = qq.UnitQuaternion()', setup=quat_setup, number=N)
-print('UnitQuaternion() :  ', t, ' us')
-t = timeit.timeit(stmt='a = qq.UnitQuaternion.Rx(0.2)', setup=quat_setup, number=N)
-print('UnitQuaternion.Rx : ', t, ' us')
-t = timeit.timeit(stmt='a = Q1 * Q2', setup=quat_setup, number=N)
-print('UnitQuaternion *:   ', t, ' us')
+    t = timeit.timeit(stmt='tr.rotx(0.2)', setup=transforms_setup, number=N)
+    print('transforms.rotx: ', t, ' us')
+
+
+    t = timeit.timeit(stmt='a = quat.qqmul(q1,q2)', setup=quat_setup, number=N)
+    print('quat.qqmul:         ', t, ' us')
+    t = timeit.timeit(stmt='a = quat.qvmul(q1,v)', setup=quat_setup, number=N)
+    print('quat.qqmul:         ', t, ' us')
+    t = timeit.timeit(stmt='a = qq.UnitQuaternion()', setup=quat_setup, number=N)
+    print('UnitQuaternion() :  ', t, ' us')
+    t = timeit.timeit(stmt='a = qq.UnitQuaternion.Rx(0.2)', setup=quat_setup, number=N)
+    print('UnitQuaternion.Rx : ', t, ' us')
+    t = timeit.timeit(stmt='a = Q1 * Q2', setup=quat_setup, number=N)
+    print('UnitQuaternion *:   ', t, ' us')
