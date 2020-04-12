198908f8ba42a2443e1146343f8a28113311d54f
Peter Corke <peter.i.corke@gmail.com>
2020-04-12 10:00:38 +1000

    almost fully functional now

--- spatialmath/quaternion.py
+++ spatialmath/quaternion.py
@@ -11,7 +11,7 @@ import argcheck
 
 class Quaternion(UserList):
     
-    def __init__(self, a1=None, a2=None, check=True):
+    def __init__(self, s=None, v=None, check=True, norm=True):
         """
         A quaternion is a compact method of representing a 3D rotation that has
         computational advantages including speed and numerical robustness.
@@ -24,22 +24,22 @@ class Quaternion(UserList):
         :param s: scalar
         :param v: vector
         """
-        if a1 is None and a2 is None:
+        if s is None and v is None:
             self.data = [ quat.qone() ]
             
-        elif argcheck.isscalar(a1) and argcheck.isvector(a2,3):
-            self.data = [ np.r_[a1, argcheck.getvector(a2)] ]
+        elif argcheck.isscalar(s) and argcheck.isvector(v,3):
+            self.data = [ np.r_[s, argcheck.getvector(v)] ]
             
-        elif argcheck.isvector(a1,4):
-            self.data = [ argcheck.getvector(a1) ]
+        elif argcheck.isvector(s,4):
+            self.data = [ argcheck.getvector(s) ]
             
-        elif type(a1) is list:
+        elif type(s) is list:
             if check:
-                assert argcheck.isvectorlist(a1,4), 'list must comprise 4-vectors'
-            self.data = [ a1 ]
+                assert argcheck.isvectorlist(s,4), 'list must comprise 4-vectors'
+            self.data = s
         
-        elif isinstance(a1, np.ndarray) and a1.shape[1] == 4:
-            self.data = [x for x in a1]
+        elif isinstance(s, np.ndarray) and s.shape[1] == 4:
+            self.data = [x for x in s]
             
         else:
             raise ValueError('bad argument to Quaternion constructor')
@@ -91,13 +91,12 @@ class Quaternion(UserList):
         if len(self) == 1:
             return self.A
         else:
-            return np.array([q.vec for q in self])
+            return np.array([q.A for q in self])
     
 
     @classmethod
-    def pure(cls, vec):
-        assert isvec(vec, 3)
-        return cls(s=0, v=vec)
+    def pure(cls, v):
+        return cls(s=0, v=argcheck.getvector(v,3), norm=True)
     
     def conj(self):
         if instance(v, np.ndarray) and len(shape) > 1 and v.shape[1] == 3:
@@ -106,7 +105,7 @@ class Quaternion(UserList):
             return self.__class__(quat.qconj(self.A))
 
     def conj(self):
-        return self.__class__( [quat.conj(q.A) for q in self] )
+        return self.__class__( [quat.qconj(q.A) for q in self] )
 
     def norm(self):
         """Return the norm of this quaternion.
@@ -137,31 +136,39 @@ class Quaternion(UserList):
     
     def __mul__(self, other):
         if type(self) == type(other):
-            pass
-        elif check.isscalar(other):
+            # quaternion * quaternion case (same class)
+            return self.__class__( self._op2(other, lambda x, y: quat.qqmul(x, y) ) )
+        elif isinstance(other, Quaternion):
+            # quaternion * quaternion case (different class)
+            return Quaternion( self._op2(other, lambda x, y: quat.qqmul(x, y) ) )
+        elif argcheck.isscalar(other):
+            # quaternion * scalar case
             print('scalar * quat')
+            return Quaternion([other*q.A for q in self])
+        elif isinstance(self, UnitQuaternion) and argcheck.isvector(other,3):
+            # scalar * vector case
+            return quat.qvmul(self.A, argcheck.getvector(other,3))
         else:
             raise ValueError('operands to * are of different types')
             
         return self._op2(other, lambda x, y: x @ y )
 
-    def __rmul__(x, y):
-        raise NotImplemented()
+    def __rmul__(self, other):
+        # scalar * quaternion case
+        return Quaternion([other*q.A for q in self])
         
     def __imul__(self, other):
         return self.__mul__(other)
     
     def __pow__(self, n):
-        assert type(n) is int, 'exponent must be an int'
-        return self.__class__([np.linalg.matrix_power(x, n) for x in self.data])
+        return self.__class__([quat.qpow(q.A, n) for q in self])
     
     def __ipow__(self, n):
         return self.__pow__(n)
                     
 
     def __truediv__(self, other):
-        assert type(self) == type(other), 'operands to * are of different types'
-        return self._op2(other, lambda x, y: x @ np.linalg.inv(y) )
+        raise NotImplemented('Quaternion division not supported')
     
 
     def __add__(self, other):
@@ -178,16 +185,19 @@ class Quaternion(UserList):
 
     def __eq__(self, other):
         assert type(self) == type(other), 'operands to == are of different types'
-        return self._op2(other, lambda x, y: np.allclose(x, y) )
+        return self._op2(other, lambda x, y: quat.qequal(x, y), list1=False )
     
     def __ne__(self, other):
-        return [not x for x in self == other]
+        return not self.__eq__(other)
     
-    def _op2(self, other, op):
+    def _op2(self, other, op, list1=True):
         
         if len(self) == 1:
             if len(other) == 1:
-                return op(self.A, other.A)
+                if list1:
+                    return [op(self.A, other.A)]
+                else:
+                    return op(self.A, other.A)
             else:
                 print('== 1xN')
                 return [op(self.A, x.A) for x in other]
@@ -197,67 +207,11 @@ class Quaternion(UserList):
                 return [op(x.A, other.A) for x in self]
             elif len(self) == len(other):
                 print('== NxN')
-                return [op(x.A, y.A) for (x,y) in zip(self.A, self.other)]
+                return [op(x.A, y.A) for (x,y) in zip(self, other)]
             else:
                 raise ValueError('length of lists to == must be same length')
                 
-
-    # def __mul__(self, other):
-    #     assert isinstance(other, Quaternion) \
-    #            or isinstance(other, int) \
-    #            or isinstance(other, float), "Can be multiplied with Quaternion, int or a float. "
-    #     if type(other) is Quaternion:
-    #         qr = Quaternion()
-    #     else:
-    #         qr = UnitQuaternion()
-    #     if isinstance(other, Quaternion):
-    #         qr.s = self.s * other.s - self.v * np.transpose(other.v)
-    #         qr.v = self.s * other.v + other.s * self.v + np.cross(self.v, other.v)
-    #     elif type(other) is int or type(other) is float:
-    #         qr.s = self.s * other
-    #         qr.v = self.v * other
-    #     return qr
-
-    # def __pow__(self, power):
-    #     """
-    #     Code retrieved from: https://github.com/petercorke/robotics-toolbox-python/blob/master/robot/Quaternion.py
-    #     Original authors: Luis Fernando Lara Tobar and Peter Corke
-    #     :param power:
-    #     :param modulo:
-    #     :return:
-    #     """
-    #     return self.__class__([quat.qpow(q.A) for q in self])
-
-    # def __imul__(self, other):
-    #     """
-    #     Code retrieved from: https://github.com/petercorke/robotics-toolbox-python/blob/master/robot/Quaternion.py
-    #     Original authors: Luis Fernando Lara Tobar and Peter Corke
-    #     :param other:
-    #     :return: self
-    #     """
-    #     if isinstance(other, Quaternion):
-    #         s1 = self.s
-    #         v1 = self.v
-    #         s2 = other.s
-    #         v2 = other.v
-
-    #         # form the product
-    #         self.s = s1 * s2 - v1 * v2.T
-    #         self.v = s1 * v2 + s2 * v1 + np.cross(v1, v2)
-
-    #     elif type(other) is int or type(other) is float:
-    #         self.s *= other
-    #         self.v *= other
-
-    #     return self
-
-    # def __add__(self, other):
-    #     assert type(self) == type(other), "Both objects should be of type: Quaternion"
-    #     return Quaternion(s=self.s + other.s, v=self.v + other.v)
-
-    # def __sub__(self, other):
-    #     assert type(self) == type(other), "Both objects should be of type: Quaternion"
-    #     return Quaternion(s=self.s - other.s, v=self.v - other.v)
+                
 
     # def __truediv__(self, other):
     #     assert isinstance(other, Quaternion) or isinstance(other, int) or isinstance(other,
@@ -302,47 +256,47 @@ class Quaternion(UserList):
     
 class UnitQuaternion(Quaternion):
     
-    def __init__(self, a1=None, a2=None, norm=True, check=True):
-        if a1 is None and a2 is None:
+    def __init__(self, s=None, v=None, norm=True, check=True):
+        if s is None and v is None:
             self.data = [ quat.qone() ]
             
-        elif argcheck.isscalar(a1) and argcheck.isvector(a2,3):
-            q = np.r_[ a1, argcheck.getvector(a2) ]
+        elif argcheck.isscalar(s) and argcheck.isvector(v,3):
+            q = np.r_[ s, argcheck.getvector(v) ]
             if norm:
                 q = quat.qunit(q)
             self.data = [q]
             
-        elif argcheck.isvector(a1,4):
+        elif argcheck.isvector(s,4):
             print('uq constructor 4vec')
-            q = argcheck.getvector(a1)
+            q = argcheck.getvector(s)
             # if norm:
             #     q = quat.qunit(q)
             print(q)
             self.data = [q]
             
-        elif type(a1) is list:
+        elif type(s) is list:
             if check:
-                assert argcheck.isvectorlist(a1,4), 'list must comprise 4-vectors'
+                assert argcheck.isvectorlist(s,4), 'list must comprise 4-vectors'
             if norm:
-                a1 = [quat.qunit(q) for q in a1]
-            self.data = a1
+                s = [quat.qunit(q) for q in s]
+            self.data = s
         
-        elif isinstance(a1, np.ndarray) and a1.shape[1] == 4:
+        elif isinstance(s, np.ndarray) and s.shape[1] == 4:
             if norm:
-                self.data = [quat.qnorm(x) for x in a1]
+                self.data = [quat.qnorm(x) for x in s]
             else:
-                self.data = [x for x in a1]
+                self.data = [x for x in s]
             
-        elif tr.isrot(a1, check=check):
-            self.data = [ quat.r2q(a1) ]
+        elif tr.isrot(s, check=check):
+            self.data = [ quat.r2q(s) ]
             
         else:
             raise ValueError('bad argument to UnitQuaternion constructor')
 
-    def __getitem__(self, i):
-        print('uq getitem', i)
-        #return self.__class__(self.data[i])
-        return self.__class__(self.data[i])
+    # def __getitem__(self, i):
+    #     print('uq getitem', i)
+    #     #return self.__class__(self.data[i])
+    #     return self.__class__(self.data[i])
     
     @property
     def R(self):
@@ -352,18 +306,18 @@ class UnitQuaternion(Quaternion):
     #-------------------------------------------- constructor variants
     @classmethod
     def Rx(cls, angle, unit='rad'):
-        return cls(rotx(angle, unit=unit))
+        return cls(tr.rotx(angle, unit=unit), check=False)
 
     @classmethod
     def Ry(cls, angle, unit='rad'):
-        return cls(roty(angle, unit=unit))
+        return cls(tr.roty(angle, unit=unit), check=False)
 
     @classmethod
     def Rz(cls, angle, unit='rad'):
-        return cls(rotz(angle, unit=unit))
+        return cls(tr.rotz(angle, unit=unit), check=False)
 
     @classmethod
-    def vec(cls, arg_in):
+    def vec3(cls, arg_in):
         assert isvec(arg_in, 3)
         s = 1 - np.linalg.norm(arg_in)
         return cls(s=s, v=arg_in)
@@ -379,15 +333,17 @@ class UnitQuaternion(Quaternion):
 
     @classmethod
     def angvec(cls, theta, v, unit='rad'):
-        assert isvec(v, 3)
-        assert type(theta) is float or type(theta) is int
-        uq = UnitQuaternion()
-        if unit == 'deg':
-            theta = theta * math.pi / 180
-        uq.s = math.cos(theta/2)
-        uq.v = math.sin(theta/2) * unitize(v)
-        return uq
+        v = argcheck.getvector(v, 3)
+        argcheck.isscalar(theta)
+        theta = argcheck.getunit(theta, unit)
+        return UnitQuaternion(s=math.cos(theta/2), v=math.sin(theta/2) * tr.unit(v), norm=False)
 
+    def __truediv__(self, other):
+        assert type(self) == type(other), 'operands to * are of different types'
+        return self._op2(other, lambda x, y: quat.qqmul(x, quat.qconj(y)) )
+    
+    def inv(self):
+        return self.__class__([quat.qconj(q.A) for q in self])
     
     @classmethod
     def omega(cls, w):
@@ -396,8 +352,6 @@ class UnitQuaternion(Quaternion):
         s = math.cos(theta / 2)
         v = math.sin(theta / 2) * unitize(w)
         return cls(s=s, v=v)
-
-
 
 
     def dot(self, omega):
@@ -473,6 +427,7 @@ class UnitQuaternion(Quaternion):
         s2 = math.sin(theta) / math.sin(theta_0)
         out = (q1 * s1) + (q2 * s2)
         return UnitQuaternion(s=float(out[0, 0]), v=out[0, 1:])
+
 
 
     def to_angvec(self, unit='rad'):
@@ -545,19 +500,8 @@ class UnitQuaternion(Quaternion):
     def rpy(cls, arg_in, unit='rad'):
         return cls.rot(rpy2r(thetas=arg_in, unit=unit))
     
-    def inv(self):
-        return Quaternion(s=self.s, v=-self.v)
 
-    @classmethod
-    def angvec(cls, theta, v, unit='rad'):
-        assert isvec(v, 3)
-        assert type(theta) is float or type(theta) is int
-        uq = UnitQuaternion()
-        if unit == 'deg':
-            theta = theta * math.pi / 180
-        uq.s = math.cos(theta/2)
-        uq.v = math.sin(theta/2) * unitize(v)
-        return uq
+
 
     @classmethod
     def omega(cls, w):
@@ -567,26 +511,7 @@ class UnitQuaternion(Quaternion):
         v = math.sin(theta / 2) * unitize(w)
         return cls(s=s, v=v)
 
-    @classmethod
-    def Rx(cls, angle, unit='rad'):
-        return cls.rot(rotx(angle, unit=unit))
 
-    @classmethod
-    def Ry(cls, angle, unit='rad'):
-        return cls.rot(roty(angle, unit=unit))
-
-    @classmethod
-    def Rz(cls, angle, unit='rad'):
-        return cls.rot(rotz(angle, unit=unit))
-
-    @classmethod
-    def vec(cls, arg_in):
-        assert isvec(arg_in, 3)
-        s = 1 - np.linalg.norm(arg_in)
-        return cls(s=s, v=arg_in)
-
-    def new(self):
-        return UnitQuaternion(s=self.s, v=self.v)
 
     def dot(self, omega):
         E = self.s * np.asmatrix(np.eye(3, 3)) - skew(self.v)
@@ -632,37 +557,7 @@ class UnitQuaternion(Quaternion):
     def matrix(self):
         pass
 
-    def interp(self, qr, r=0.5, shortest=False):
-        """
-        Algorithm source: https://en.wikipedia.org/wiki/Slerp
-        :param qr: UnitQuaternion
-        :param shortest: Take the shortest path along the great circle
-        :param r: interpolation point
-        :return: interpolated UnitQuaternion
-        """
-        assert type(qr) is UnitQuaternion
-        if self == qr:
-            return self
 
-        q1 = self.double()
-        q2 = qr.double()
-        dot = q1*np.transpose(q2)
-
-        # If the dot product is negative, the quaternions
-        # have opposite handed-ness and slerp won't take
-        # the shorter path. Fix by reversing one quaternion.
-        if shortest:
-            if dot < 0:
-                q1 = - q1
-                dot = -dot
-
-        dot = np.clip(dot, -1, 1)  # Clip within domain of acos()
-        theta_0 = math.acos(dot)  # theta_0 = angle between input vectors
-        theta = theta_0 * r  # theta = angle between v0 and result
-        s1 = float(math.cos(theta) - dot * math.sin(theta) / math.sin(theta_0))
-        s2 = math.sin(theta) / math.sin(theta_0)
-        out = (q1 * s1) + (q2 * s2)
-        return UnitQuaternion(s=float(out[0, 0]), v=out[0, 1:])
 
     def to_vec(self):
         if self.s < 0:
@@ -725,10 +620,32 @@ q.append(q)
 print(len(q))
 print(q)
 
-a = np.random.uniform(size=(1,4))
+a = np.random.uniform(size=(6,4))
 q = Quaternion(a)
+q2 = Quaternion()
 
 u = UnitQuaternion()
 #print(u)
 len(u)
 a = u[0]
+
+if __name__ == '__main__':
+
+    import numpy.testing as nt
+    import unittest
+    
+    from transforms import *
+            
+    class TestUnitQuaternion(unittest.TestCase):
+        
+        def test_constructor(self):
+            nt.assert_array_almost_equal(UnitQuaternion().vec, np.r_[1,0,0,0])
+            
+            nt.assert_array_almost_equal(UnitQuaternion.Rx(90,'deg').vec, np.r_[1,1,0,0]/math.sqrt(2))
+            nt.assert_array_almost_equal(UnitQuaternion.Rx(-90,'deg').vec, np.r_[1,-1,0,0]/math.sqrt(2))
+            nt.assert_array_almost_equal(UnitQuaternion.Ry(90,'deg').vec, np.r_[1,0,1,0]/math.sqrt(2))
+            nt.assert_array_almost_equal(UnitQuaternion.Ry(-90,'deg').vec, np.r_[1,0,-1,0]/math.sqrt(2))
+            nt.assert_array_almost_equal(UnitQuaternion.Rz(90,'deg').vec, np.r_[1,0,0,1]/math.sqrt(2))
+            nt.assert_array_almost_equal(UnitQuaternion.Rz(-90,'deg').vec, np.r_[1,0,0,-1]/math.sqrt(2))
+            
+    unittest.main()
