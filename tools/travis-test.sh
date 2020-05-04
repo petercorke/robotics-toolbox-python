6312b56a5d9f9a9c9effb73b46294de814b96d53
jhavl <jhavl@users.noreply.github.com>
2020-05-05 09:59:07 +1000

    Create travis-test.sh

---
+++ tools/travis-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+source builds/venv/bin/activate
+
+# travis venv tests override python
+PYTHON=${PYTHON:-python}
+PIP=${PIP:-pip}
+
+setup_base()
+{
+    $PIP install -v .
+}
+
+run_test()
+{
+  export PYTHONWARNINGS="ignore::DeprecationWarning:virtualenv"
+}
