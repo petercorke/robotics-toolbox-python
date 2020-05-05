4471e56c8fac91968e8f87260194803785b779e7
micah-huth <micah.huth@iinet.net.au>
2020-05-05 16:22:52 +1000

    Added line creation

--- graphics/generic.py
+++ graphics/generic.py
@@ -4,6 +4,7 @@ Generic Drawing Functions
 """
 
 from vpython import *
+from math import sqrt
 
 '''
 1. init conditions of scene
@@ -53,7 +54,7 @@ def draw_grid():
     Current zoom setting to show axis 5 units away.
     TODO: update a changeable zoom setting.
     """
-
+    create_grid()
     return
 
 
@@ -70,3 +71,43 @@ def draw_reference_frame_axes():
     Each axis is represented in the objects reference frame.
     """
     return
+
+
+def create_grid():
+    l1 = create_line([-5, 0, 0], [5, 0, 0])
+    sphere(pos=vector(-5, 0, 0), radius=0.5, color=color.red)
+    sphere(pos=vector(5, 0, 0), radius=0.5, color=color.green)
+    return
+
+
+def create_line(pos1, pos2):
+    """
+    Create a line from position 1 to position 2
+    @type pos1: int array
+    @param pos1: 3D position of one end of the line
+    @type pos2: int array
+    @param pos2: 3D position of the other end of the line
+    TODO: insert checks to ensure 3D points given
+    """
+
+    # Length of the line using trigonometry
+    line_len = sqrt(
+        (pos2[0] - pos1[0])**2 +
+        (pos2[1] - pos1[1])**2 +
+        (pos2[2] - pos1[2])**2
+    )
+
+    # Position of the line is the midpoint (centre) between the ends
+    position = vector(
+        (pos1[0] + pos2[0]) / 2,
+        (pos1[1] + pos2[1]) / 2,
+        (pos1[2] + pos2[2]) / 2,
+    )
+
+    # return a box of thin width and height to resemble a line
+    return box(pos=position, length=line_len, width=0.01, height=0.01, color=color.black)
+
+
+if __name__ == "__main__":
+    print("Graphics Test")
+    init_canvas(grid=True)
