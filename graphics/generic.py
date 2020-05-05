25bff65c90db988fe51f9ac51ad53d55da931c61
micah-huth <micah.huth@iinet.net.au>
2020-05-05 14:49:03 +1000

    Initial commit

---
+++ graphics/generic.py
@@ -0,0 +1,29 @@
+"""
+Generic Drawing Functions
+
+"""
+
+from vpython import *
+
+'''
+1. init conditions of scene
+2. draw axes
+3. draw labels
+4. draw reference frames
+'''
+
+
+def init_canvas(height=500, width=500, title='', caption='', grid=False):
+    return
+
+
+def draw_grid():
+    return
+
+
+def draw_label():
+    return
+
+
+def draw_reference_frame_axes():
+    return
