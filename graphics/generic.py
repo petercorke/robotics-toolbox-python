c50410413ae90274343960c832f272d4fd5a7c47
micah-huth <micah.huth@iinet.net.au>
2020-05-12 10:34:30 +1000

    Updated all comments. Some minor refactoring.

--- graphics/generic.py
+++ graphics/generic.py
@@ -7,6 +7,11 @@ from vpython import *
 from numpy import sign
 from math import sqrt
 
+# TODO (NEXT TIME OPENING)
+#  1. Comments
+#  2. Read other TODOs
+#  3. Split into diff files
+#  4. Push to github
 
 # TODO: Additions:
 #  1. Keyboard input to maneuver around 3D map
@@ -23,32 +28,39 @@ def init_canvas(height=500, width=1000, title='', caption='', grid=True):
         - Width, height
         - Title, caption
         - Axes drawn (if applicable)
-    TODO: Add others as available
     
-    @type height: int (pixels)
-    @param height: Height of the canvas on screen.
-    @type width: int (pixels)
-    @param width: Width of the canvas on screen.
-    @type title: str
-    @param title: Title of the plot.
-    @type caption: str
-    @param caption: Caption (subtitle) of the plot.
-    @type grid: bool
-    @param grid: Whether a grid should be displayed in the plot.
+    :param height: Height of the canvas on screen (Pixels), defaults to 500.
+    :type height: int, optional
+    :param width: Width of the canvas on screen (Pixels), defaults to 1000.
+    :type width: int, optional
+    :param title: Title of the plot. Gets displayed above canvas, defaults to ''.
+    :type title: str, optional
+    :param caption: Caption (subtitle) of the plot. Gets displayed below the canvas, defaults to ''.
+    :type caption: str, optional
+    :param grid: Whether a grid should be displayed in the plot, defaults to True.
+    :type grid: bool, optional
+    :return: Returns an array of the grid and the numbers displayed along the grid.
+    :rtype: Array of vpython objects: [compound object, array of labels]
     """
+
+    # Apply the settings
     scene.background = color.white
     scene.width = width
     scene.height = height
     scene.autoscale = False
+
     if title != '':
         scene.title = title
+
     if caption != '':
         scene.caption = caption
-    if grid:
-        plot_grid = draw_grid()
-        draw_reference_frame_axes(vector(0, 0, 0), vector(1, 0, 0), radians(0))
-    else:
-        plot_grid = box().visible = False
+
+    plot_grid = draw_grid()
+    if not grid:
+        # If no grid, turn the objects invisible
+        plot_grid[0].visible = False
+        for number in plot_grid[1]:
+            number.visible = False
 
     return plot_grid
 
@@ -56,10 +68,13 @@ def init_canvas(height=500, width=1000, title='', caption='', grid=True):
 def draw_grid():
     """
     Display grids along the x, y, z axes.
+
+    :return: Returns an array of the grid and the numbers displayed along the grid.
+    :rtype: Array: [compound_object, array_of_labels]
     """
     # TODO: Only update if camera has moved/zoomed/rotated
-    num_squares = 10
-    relative_cam = True
+    num_squares = 10  # Length of the grid in each direction (in units)
+    relative_cam = True  # Whether the grid follows the camera rotation and movement
 
     the_grid = create_grid(relative_cam, num_squares)
     the_numbers = create_grid_numbers(relative_cam, num_squares)
@@ -69,10 +84,18 @@ def draw_grid():
 
 def draw_label(label_text, label_position):
     """
-    Display a label at a given position
+    Display a label at a given position, with borders and lines
+
+    :param label_text: String of text to be written on the label.
+    :type label_text: str
+    :param label_position: 3D vector position to draw the label at.
+    :type label_position: vpython.vector
+    :return: The created label object.
+    :rtype: vpython.label
     """
     # TODO: Sanity check param input
 
+    # Custom settings for the label
     label_height = 10
     label_xoffset = 0
     label_yoffset = 50
@@ -98,15 +121,23 @@ def draw_label(label_text, label_position):
 
 def draw_text(label_text, label_position):
     """
-    Display a label at a given position
+    Display a label at a given position, without borders or lines.
+
+    :param label_text: String of text to be written on the label.
+    :type label_text: str
+    :param label_position: 3D vector position to draw the label at.
+    :type label_position: vpython.vector
+    :return: The created label object.
+    :rtype: vpython.label
     """
     # TODO: Sanity check param input
 
+    # Distance of camera from focus point to determine text size
     distance_from_center = mag(scene.center - scene.camera.pos)
 
-    # Far away = smaller, closer = larger (up to a min (20) and max (40))
+    # Far away = smaller text, closer = larger text (up to a min (20) and max (40))
     # Typically 5->20 units away
-    # (eqn and limits modified to suit display) = -1.3333 * distance_from_center + 46.6667
+    # (eqn and limits modified to suit display better) = -1.3333 * distance_from_center + 46.6667
     label_height = -1.3333 * distance_from_center + 36.6667  # Calculate label height
     label_height = max(min(label_height, 35), 10)  # Limit to 10->35
     label_xoffset = 0
@@ -139,6 +170,15 @@ def draw_reference_frame_axes(origin, x_axis_vector, x_axis_rotation):
     """
     Draw x, y, z axes from the given point.
     Each axis is represented in the objects reference frame.
+
+    :param origin: 3D vector representing the point to draw the reference from at.
+    :type origin: vpython.vector
+    :param x_axis_vector: 3D vector representing the direction of the positive x axis.
+    :type x_axis_vector: vpython.vector
+    :param x_axis_rotation: Angle in radians to rotate the frame around the x-axis.
+    :type x_axis_rotation: float
+    :return: Compound object of the 3 axis arrows.
+    :rtype: vpython.compound
     """
 
     # TODO: Remove once verified
@@ -150,7 +190,7 @@ def draw_reference_frame_axes(origin, x_axis_vector, x_axis_rotation):
 
     # Create Basic Frame
     # Draw X Axis
-    x_arrow = arrow(pos=origin, axis=vector(1, 0, 0), length=1, color=color.red)  # In direction of tooltip/object
+    x_arrow = arrow(pos=origin, axis=vector(1, 0, 0), length=1, color=color.red)
 
     # Draw Y Axis
     y_arrow = arrow(pos=origin, axis=vector(0, 1, 0), length=1, color=color.green)
@@ -167,44 +207,46 @@ def draw_reference_frame_axes(origin, x_axis_vector, x_axis_rotation):
     frame_ref.axis = x_axis_vector
     frame_ref.rotate(angle=x_axis_rotation)
 
-    return
+    return frame_ref
 
 
 def create_grid(bool_camera_relative, num_squares):
     """
-    Draw a grid along each 3D plane
+    Draw a grid along each 3D plane, that is closest to the camera.
+
+    :param bool_camera_relative: Whether to draw the axes at the camera focus point or at (0, 0, 0).
+    :type bool_camera_relative: bool
+    :param num_squares: How many unit squares to draw along the axis.
+    :type num_squares: int
+    :return: Vpython compound object of the three drawn axes.
+    :rtype: vpython.compound
     """
-    # TODO: create an update_grid() that updates grid min/max relative to camera position(/rotation?).
-    #  And/or ensure grid walls are at the end of visible range (relative to camera axis (find a relative origin to
-    #  draw from)). May have to be in this one function?? Can't uncompound objects?
-    #  Another option is to have grid in constant position relative to camera, except the numbers on the grid change
-    #  relative to position (place grids against walls (like MATLAB 3D plots))
-
-    # TODO: Change array input to create_line to vector inputs. Vector has functions to utilise (mag, etc)
-
-    # TODO: Using scene.camera.axis, work out whether each plane should be +ve or -ve numbers
-    #   AXIS |  GRID | XZ | XY | YZ
-    #  -,-,- | +,+,+ | ++ | ++ | ++
-    #  -,-,+ | +,+,- | +- | ++ | +-
-    #  -,+,- | +,-,+ | ++ | +- | -+
-    #  -,+,+ | +,-,- | +- | +- | --
-    #  +,-,- | -,+,+ | -+ | -+ | ++
-    #  +,-,+ | -,+,- | -- | -+ | +-
-    #  +,+,- | -,-,+ | -+ | -- | -+
-    #  +,+,+ | -,-,- | -- | -- | --
 
     # Initial conditions
     xz_lines = []
     xy_lines = []
     yz_lines = []
+    camera_axes = scene.camera.axis
+    # Locate centre of axes
     if bool_camera_relative:
         x_origin, y_origin, z_origin = round(scene.center.x), round(scene.center.y), round(scene.center.z)
     else:
         x_origin, y_origin, z_origin = 0, 0, 0
-    camera_axes = scene.camera.axis
 
-    # min = -num_squares or 0 around default position
-    # max = +num_squares or 0 around default position
+    #   CAMERA AXES |  DISPLAYED GRID | XZ PLANE | XY PLANE | YZ PLANE
+    #      x,y,z    |      x,y,z      |   x,z    |    x,y   |    y,z
+    #  -------------+-----------------+----------+----------+----------
+    #      -,-,-    |      +,+,+      |   +,+    |    +,+   |    +,+
+    #      -,-,+    |      +,+,-      |   +,-    |    +,+   |    +,-
+    #      -,+,-    |      +,-,+      |   +,+    |    +,-   |    -,+
+    #      -,+,+    |      +,-,-      |   +,-    |    +,-   |    -,-
+    #      +,-,-    |      -,+,+      |   -,+    |    -,+   |    +,+
+    #      +,-,+    |      -,+,-      |   -,-    |    -,+   |    +,-
+    #      +,+,-    |      -,-,+      |   -,+    |    -,-   |    -,+
+    #      +,+,+    |      -,-,-      |   -,-    |    -,-   |    -,-
+    # min = -num_squares or 0, around the default position
+    # max = +num_squares or 0, around the default position
+    # e.g. at the origin, for negative axes: -10 -> 0, positive axes: 0 -> 10
     min_x_coord = x_origin + int(-(num_squares / 2) + (sign(camera_axes.x) * -1) * (num_squares / 2))
     max_x_coord = x_origin + int((num_squares / 2) + (sign(camera_axes.x) * -1) * (num_squares / 2))
 
@@ -216,23 +258,29 @@ def create_grid(bool_camera_relative, num_squares):
 
     # XZ plane
     for x_point in range(min_x_coord, max_x_coord + 1):
+        # Draw a line across for each x coord, along the same y-axis, from min to max z coord
         xz_lines.append(create_line([x_point, y_origin, min_z_coord], [x_point, y_origin, max_z_coord]))  # x-axis
     for z_point in range(min_z_coord, max_z_coord + 1):
+        # Draw a line across each z coord, along the same y-axis, from min to max z coord
         xz_lines.append(create_line([min_x_coord, y_origin, z_point], [max_x_coord, y_origin, z_point]))  # z-axis
 
     # XY plane
     for x_point in range(min_x_coord, max_x_coord + 1):
+        # Draw a line across each x coord, along the same z-axis, from min to max y coord
         xy_lines.append(create_line([x_point, min_y_coord, z_origin], [x_point, max_y_coord, z_origin]))  # x-axis
     for y_point in range(min_y_coord, max_y_coord + 1):
+        # Draw a line across each y coord, along the same z-axis, from min to max x coord
         xy_lines.append(create_line([min_x_coord, y_point, z_origin], [max_x_coord, y_point, z_origin]))  # y-axis
 
     # YZ plane
     for y_point in range(min_y_coord, max_y_coord + 1):
+        # Draw a line across each y coord, along the same x-axis, from min to max z coord
         yz_lines.append(create_line([x_origin, y_point, min_z_coord], [x_origin, y_point, max_z_coord]))  # y-axis
     for z_point in range(min_z_coord, max_z_coord + 1):
+        # Draw a line across each z coord, along the same x-axis, from min to max y coord
         yz_lines.append(create_line([x_origin, min_y_coord, z_point], [x_origin, max_y_coord, z_point]))  # z-axis
 
-    # Compound the lines together into one object
+    # Compound the lines together into respective objects
     xz_plane = compound(xz_lines)
     xy_plane = compound(xy_lines)
     yz_plane = compound(yz_lines)
@@ -244,16 +292,40 @@ def create_grid(bool_camera_relative, num_squares):
 
 
 def create_grid_numbers(bool_camera_relative, num_squares):
-    padding = 0.25
-    camera_axes = scene.camera.axis
+    """
+    Draw the grid numbers along the xyz axes.
 
+    :param bool_camera_relative: Whether to draw the axes at the camera focus point or at (0, 0, 0).
+    :type bool_camera_relative: bool
+    :param num_squares: How many unit squares to draw along the axis.
+    :type num_squares: int
+    :return: An array of labels
+    :rtype: vpython.label array
+    """
+
+    # Initial conditions
+    padding = 0.25  # Padding to not draw numbers on top of lines.
+    camera_axes = scene.camera.axis
+    # Locate center of the axes
     if bool_camera_relative:
         x_origin, y_origin, z_origin = round(scene.center.x), round(scene.center.y), round(scene.center.z)
     else:
         x_origin, y_origin, z_origin = 0, 0, 0
 
-    # min = -num_squares or 0 around default position
-    # max = +num_squares or 0 around default position
+    #   CAMERA AXES |  DISPLAYED GRID | XZ PLANE | XY PLANE | YZ PLANE
+    #      x,y,z    |      x,y,z      |   x,z    |    x,y   |    y,z
+    #  -------------+-----------------+----------+----------+----------
+    #      -,-,-    |      +,+,+      |   +,+    |    +,+   |    +,+
+    #      -,-,+    |      +,+,-      |   +,-    |    +,+   |    +,-
+    #      -,+,-    |      +,-,+      |   +,+    |    +,-   |    -,+
+    #      -,+,+    |      +,-,-      |   +,-    |    +,-   |    -,-
+    #      +,-,-    |      -,+,+      |   -,+    |    -,+   |    +,+
+    #      +,-,+    |      -,+,-      |   -,-    |    -,+   |    +,-
+    #      +,+,-    |      -,-,+      |   -,+    |    -,-   |    -,+
+    #      +,+,+    |      -,-,-      |   -,-    |    -,-   |    -,-
+    # min = -num_squares or 0, around the default position
+    # max = +num_squares or 0, around the default position
+    # e.g. at the origin, for negative axes: -10 -> 0, positive axes: 0 -> 10
     min_x_coord = x_origin + int(-(num_squares / 2) + (sign(camera_axes.x) * -1) * (num_squares / 2))
     max_x_coord = x_origin + int((num_squares / 2) + (sign(camera_axes.x) * -1) * (num_squares / 2))
 
@@ -267,7 +339,9 @@ def create_grid_numbers(bool_camera_relative, num_squares):
 
     # X plane
     for x_pos in range(min_x_coord, max_x_coord + sign(max_x_coord)):
+        # Draw the corresponding unit number at each x coordinate
         nums.append(draw_text(str(x_pos), vector(x_pos + padding, y_origin + padding, z_origin)))
+    # Draw the axis label at either the positive or negative side
     if (sign(camera_axes.x) * -1) > 0:
         nums.append(draw_text("X", vector(max_x_coord + 1, y_origin, z_origin)))
     else:
@@ -275,7 +349,9 @@ def create_grid_numbers(bool_camera_relative, num_squares):
 
     # Y plane
     for y_pos in range(min_y_coord, max_y_coord + sign(max_y_coord)):
+        # Draw the corresponding unit number at each y coordinate
         nums.append(draw_text(str(y_pos), vector(x_origin, y_pos + padding, z_origin + padding)))
+    # Draw the axis label at either the positive or negative side
     if (sign(camera_axes.y) * -1) > 0:
         nums.append(draw_text("Y", vector(x_origin, max_y_coord + 1, z_origin)))
     else:
@@ -283,7 +359,9 @@ def create_grid_numbers(bool_camera_relative, num_squares):
 
     # Z plane
     for z_pos in range(min_z_coord, max_z_coord + sign(max_z_coord)):
+        # Draw the corresponding unit number at each z coordinate
         nums.append(draw_text(str(z_pos), vector(x_origin, y_origin - padding, z_pos + padding)))
+    # Draw the axis label at either the positive or negative side
     if (sign(camera_axes.z) * -1) > 0:
         nums.append(draw_text("Z", vector(x_origin, y_origin, max_z_coord + 1)))
     else:
@@ -294,14 +372,15 @@ def create_grid_numbers(bool_camera_relative, num_squares):
 
 def create_line(pos1, pos2):
     """
-    Create a line from position 1 to position 2
-    @type pos1: int array
-    @param pos1: 3D position of one end of the line
-    @type pos2: int array
-    @param pos2: 3D position of the other end of the line
+    Create a line from position 1 to position 2.
+
+    :param pos1: 3D position of one end of the line.
+    :type pos1: int array
+    :param pos2: 3D position of the other end of the line.
+    :type pos2: int array
     """
     # TODO: Insert checks to ensure 3D points given (e.g. index out of bounds error if given 2D points)
-    # TODO: Maybe add a colour input??
+    # TODO: Change array input to create_line() to vector inputs. Vector has functions to utilise (mag, etc)
 
     # Length of the line using trigonometry
     line_len = sqrt(
@@ -328,7 +407,7 @@ def create_line(pos1, pos2):
     thickness = 0.01
     return box(pos=position, axis=axis_dir, length=line_len, width=thickness, height=thickness, color=color.black)
 
-
+# TODO: Remove/Move to new file
 def testing_references():
     le = 0.8
 
