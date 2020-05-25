129396469d11958623536744adc5908db42c27ff
micah-huth <micah.huth@iinet.net.au>
2020-05-25 11:21:45 +1000

    Fixed link5 creation. Also implemented import_puma_560

--- graphics/model_puma560.py
+++ graphics/model_puma560.py
@@ -3,7 +3,22 @@ from graphics.graphics_stl import *
 
 
 def import_puma_560():
-    pass
+
+    puma560 = Robot(
+        [
+            create_link_0(),
+            create_link_1(),
+            create_link_2(),
+            create_link_3(),
+            create_link_4(),
+            create_link_5(),
+            create_link_6()
+        ]
+    )
+
+    puma560.set_reference_visibility(False)
+
+    return puma560
 
 
 def create_link_0():
@@ -31,7 +46,8 @@ def create_link_1():
     # Orient the object so that it's origin and toolpoint in known locations
     # This way, rotations are relative to the correct 3D position of the object
     stl_obj.rotate(angle=radians(90), axis=vector(0, 1, 0), origin=vector(0, 0, 0))
-    stl_obj.rotate(angle=radians(90), axis=vector(1, 0, 0), origin=vector(0, 0, 0))
+    stl_obj.rotate(angle=radians(90), axis=vector(0, 0, 1), origin=vector(0, 0, 0))
+    stl_obj.rotate(angle=radians(90), axis=vector(0, 1, 0), origin=vector(0, 0, 0))
     stl_obj_z_origin = -stl_obj.height / 2
     stl_obj_current_origin_location = vector(stl_obj.pos.x, stl_obj.pos.y, stl_obj_z_origin)
     stl_obj_required_origin_location = vector(stl_obj.pos.x, stl_obj.pos.y, 0)
@@ -42,7 +58,7 @@ def create_link_1():
     # Create the robot link
     connection_from = vector(0, 0, 0)
     # Numbers come from previous adjustments, plus extra observed in meshlab
-    connection_to = vector(0.184, 0, stl_obj.height/2)
+    connection_to = vector(0, 0.184, stl_obj.width / 2)
     link = RotationalJoint(connection_from, connection_to, vector(1, 0, 0), stl_obj)
     return link
 
@@ -75,7 +91,7 @@ def create_link_3():
     # Orient the object so that it's origin and toolpoint in known locations
     # This way, rotations are relative to the correct 3D position of the object
     stl_obj.rotate(angle=radians(90), axis=vector(0, 1, 0), origin=vector(0, 0, 0))
-    stl_obj_y_origin = -stl_obj.height / 2
+    stl_obj_y_origin = stl_obj.height / 2
     stl_obj_x_origin = -0.05
     stl_obj_current_origin_location = vector(stl_obj_x_origin, stl_obj_y_origin, stl_obj.pos.z)
     stl_obj_required_origin_location = vector(0, 0, stl_obj.pos.z)
@@ -86,7 +102,7 @@ def create_link_3():
     # Create the robot link
     connection_from = vector(0, 0, 0)
     # Numbers come from previous adjustments, plus extra observed in meshlab
-    connection_to = vector(0.36+0.05, stl_obj.height/2, 0)
+    connection_to = vector(0.36 + 0.05, -stl_obj.height / 2, 0)
     link = RotationalJoint(connection_from, connection_to, vector(1, 0, 0), stl_obj)
     return link
 
@@ -117,8 +133,11 @@ def create_link_5():
     stl_obj = import_object_from_stl('link5')
     # Orient the object so that it's origin and toolpoint in known locations
     # This way, rotations are relative to the correct 3D position of the object
-    stl_obj.rotate(angle=radians(-90), axis=vector(1, 0, 0), origin=vector(0, 0, 0))
-    stl_obj.rotate(angle=radians(-90), axis=vector(0, 0, 1), origin=vector(0, 0, 0))
+    stl_obj.rotate(angle=radians(90), axis=vector(1, 0, 0), origin=vector(0, 0, 0))
+    stl_obj.rotate(angle=radians(90), axis=vector(0, 0, 1), origin=vector(0, 0, 0))
+    stl_obj_current_origin_location = vector(0, 0, 0)
+    stl_obj_required_origin_location = vector(0, 0, 0)
+    stl_obj = set_stl_origin(stl_obj, stl_obj_current_origin_location, stl_obj_required_origin_location)
     # Change color
     stl_obj.color = color.yellow
 
@@ -141,7 +160,7 @@ def create_link_6():
     stl_obj_required_origin_location = vector(0, stl_obj.pos.y, stl_obj.pos.z)
     stl_obj = set_stl_origin(stl_obj, stl_obj_current_origin_location, stl_obj_required_origin_location)
     # Change color
-    stl_obj.color = color.red
+    stl_obj.color = color.black
 
     # Create the robot link
     connection_from = vector(0, 0, 0)
