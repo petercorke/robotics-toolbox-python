5411c273d91ddc5662420629b64d55a40d8f670c
micah-huth <micah.huth@iinet.net.au>
2020-06-21 14:47:30 +1000

    Updated tester functions and added comments.

--- graphics/graphics_test_features.py
+++ graphics/graphics_test_features.py
@@ -1,108 +1,65 @@
 from graphics.model_puma560 import *
 
 
-def test_grid():
+def test_grid_updating():
+    """
+    This test will create a canvas and update the grid every second.
+    Eventually, the grid will only update through callbacks of key/button releases.
+    """
     canvas_grid = init_canvas()
     while True:
         sleep(1)
         canvas_grid.update_grid()
-        print("XY:", canvas_grid.grid_object[0].pos)
 
 
-def test_reference_frames():
-    le = 0.2
+def test_reference_frame_pose():
+    """
+    This test will create a canvas, and place reference frames at the given positions and orientations.
+    Each frame must be manually inspected for validity.
+    """
     canvas_grid = init_canvas()
 
     # Test 1 | Position (0, 0, 0), Axis (1, 0, 0), No Rotation
-    # Drawn
     draw_reference_frame_axes(vector(0, 0, 0), vector(1, 0, 0), radians(0))
-    # Actual
-    #arrow(pos=vector(0, 0, 0), axis=vector(1, 0, 0), length=le, color=color.purple)
 
     # Test 2 | Position (1, 1, 1), Axis (0, 0, 1), No Rotation
-    # Drawn
     draw_reference_frame_axes(vector(1, 1, 1), vector(0, 0, 1), radians(0))
-    # Actual
-    #arrow(pos=vector(1, 1, 1), axis=vector(0, 0, 1), length=le, color=color.purple)
 
     # Test 3 | Position (2, 2, 2), Axis (1, 0, 0), 30 deg rot
-    # Drawn
     draw_reference_frame_axes(vector(2, 2, 2), vector(1, 0, 0), radians(30))
-    # Actual
-    #arrow(pos=vector(2, 2, 2), axis=vector(1, 0, 0), length=le, color=color.purple).rotate(radians(30))
 
     # Test 4 | Position (3, 3, 3), Axis (1, 1, 1), No Rotation
-    # Drawn
     draw_reference_frame_axes(vector(3, 3, 3), vector(1, 1, 1), radians(0))
-    # Actual
-    #arrow(pos=vector(3, 3, 3), axis=vector(1, 1, 1), length=le, color=color.purple)
 
     # Test 5 | Position (4, 4, 4), Axis (1, 1, 1), 30 deg rot
-    # Drawn
     draw_reference_frame_axes(vector(4, 4, 4), vector(1, 1, 1), radians(30))
-    # Actual
-    #arrow(pos=vector(4, 4, 4), axis=vector(1, 1, 1), length=le, color=color.purple).rotate(radians(30))
 
     # Test 6 | Position (5, 5, 5), Axis (2, -1, 4), No Rotation
-    # Drawn
     draw_reference_frame_axes(vector(5, 5, 5), vector(2, -1, 4), radians(0))
-    # Actual
-    #arrow(pos=vector(5, 5, 5), axis=vector(2, -1, 4), length=le, color=color.purple)
 
     # Test 7 | Position (6, 6, 6), Axis (2, -1, 4), 30 deg rot
-    # Drawn
     draw_reference_frame_axes(vector(6, 6, 6), vector(2, -1, 4), radians(30))
-    # Actual
-    #arrow(pos=vector(6, 6, 6), axis=vector(2, -1, 4), length=le, color=color.purple).rotate(radians(30))
 
 
 def test_import_stl():
+    """
+    This test will create a canvas with the Puma560 model loaded in.
+    The robot should have all joint angles to 0 (Robot is in an 'L' shape from (1, 1, 0) in the +X axis direction)
+    """
     canvas_grid = init_canvas()
 
     puma560 = import_puma_560()
     puma560.move_base(vector(1, 1, 0))
 
-    """
-    puma560.set_reference_visibility(False)
-    puma560.print_joint_angles(True)
-
-    sleep(2)
-    puma560.set_joint_angle(4, radians(35))
-
-    sleep(2)
-    puma560.set_joint_angle(2, radians(-56))
-
-    sleep(2)
-    puma560.set_joint_angle(0, radians(78))
-
-    sleep(2)
-    puma560.set_all_joint_angles([
-        0, 0, 0, 0, 0, 0, 0
-    ])
-
-    
-    sleep(2)
-    puma560.set_all_joint_angles([
-        radians(45), 0, 0, 0, 0, 0, 0,
-    ])
-
-    sleep(2)
-    puma560.set_all_joint_angles([
-        radians(45), 0, radians(-90), 0, 0, 0, 0,
-    ])
-    
-    sleep(2)
-    puma560.set_joint_angle(4, radians(156))
-
-    sleep(2)
-    puma560.set_joint_angle(2, radians(-23))
-    
-
-    puma560.print_joint_angles(True)
-    """
-
 
 def test_rotational_link():
+    """
+    This test will create a simple rotational link from (0, 0, 0) to (1, 0, 0).
+    Depending on which for loop is commented out:
+    The joint will then rotate in a positive direction about it's +y axis.
+    OR
+    The joint will rotate to the given angles in the list.
+    """
     canvas_grid = init_canvas()
     # rot = vector(0, 0, 1)
     rot = vector(0, 1, 0)
@@ -113,23 +70,62 @@ def test_rotational_link():
 
     # for angle in [0, 45, 90, 135, 180, -135, -90, -45, 33, -66, -125, 162, 360, 450, -270, -333]:
     #    sleep(5)
+    #    rot_link.rotate_joint(radians(angle))
+
     for angle in range(0, 360):
         sleep(0.05)
         rot_link.rotate_joint(radians(angle))
 
 
-def test_graphical_robot():
+def test_graphical_robot_creation():
+    """
+    This test will create a simple 3-link graphical robot.
+    The joints are then set to particular angles to show rotations.
+    """
     canvas_grid = init_canvas()
 
     x = GraphicalRobot([
-        RotationalJoint(vector(0, 0, 0), vector(1, 1, 1))
+        RotationalJoint(vector(0, 0, 0), vector(1, 0, 0)),
+        RotationalJoint(vector(1, 0, 0), vector(2, 0, 0)),
+        RotationalJoint(vector(2, 0, 0), vector(3, 0, 0))
     ])
 
-    x.set_joint_angle(0, radians(20))
+    sleep(2)
+    x.set_all_joint_angles([radians(-45), radians(45), radians(15)])
 
 
-def test_place_joint():
-    pass
+def test_puma560_angle_change():
+    """
+    This test loads in the Puma560 model and changes its angles over time.
+    Joint angles are printed for validation.
+    """
+    canvas_grid = init_canvas()
+
+    puma560 = import_puma_560()
+    puma560.move_base(vector(1, 1, 0))
+
+    puma560.set_reference_visibility(False)
+    print("Prior Angles")
+    puma560.print_joint_angles(True)
+
+    sleep(2)
+    puma560.set_all_joint_angles([
+        radians(45), 0, 0, 0, 0, 0, 0,
+    ])
+
+    sleep(2)
+    puma560.set_all_joint_angles([
+        radians(45), 0, radians(-90), 0, 0, 0, 0,
+    ])
+
+    sleep(2)
+    puma560.set_joint_angle(4, radians(156))
+
+    sleep(2)
+    puma560.set_joint_angle(2, radians(-23))
+
+    print("Final Angles")
+    puma560.print_joint_angles(True)
 
 
 def test_animate_joints():
