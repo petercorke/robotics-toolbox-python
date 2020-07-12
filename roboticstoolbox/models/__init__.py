e8da1c00dd1a182c5f7b5bf3d1b9ef952a07445c
Peter Corke <peter.i.corke@gmail.com>
2020-07-13 08:46:11 +1000

    added Puma model to list

--- roboticstoolbox/models/__init__.py
+++ roboticstoolbox/models/__init__.py
@@ -1,8 +1,10 @@
 # Robot Models
 from roboticstoolbox.models.Panda import Panda
+from roboticstoolbox.models.puma560 import Puma560
 # from roboticstoolbox.models.PandaMDH import PandaMDH
 
 __all__ = [
-    'Panda'
+    'Panda',
+    'Puma560',
     # 'PandaMDH'
 ]
