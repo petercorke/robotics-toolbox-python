46b07e38e0d8ab2f080398b5a3b88a1bb97b8a13
micah-huth <micah.huth@iinet.net.au>
2020-07-27 13:59:07 +1000

    Modified changes from last commit

--- tests/manual_testing_graphics.py
+++ tests/manual_testing_graphics.py
@@ -94,36 +94,36 @@ def test_graphical_robot_creation():
     """
     p = SE3()
 
-    p1 = p.Ty(1)
-    p2 = p.Ty(2)
-    p3 = p.Ty(3)
+    p1 = p.Tx(1)
+    p2 = p.Tx(2)
+    p3 = p.Tx(3)
 
     robot = gph.GraphicalRobot(g_canvas, 'test_3_link_robot')
 
-    robot.append_link('r', p1, 1.0)
-    robot.append_link('R', p2, 1.0)
-    robot.append_link('r', p3, 1.0)
+    robot.append_link('r', p1, 1.0, axis_through=array([1, 0, 0]))
+    robot.append_link('R', p2, 1.0, axis_through=array([1, 0, 0]))
+    robot.append_link('r', p3, 1.0, axis_through=array([1, 0, 0]))
 
     arr = array([
-        [1, 0, 0, 0],
-        [0, 0, -1, 0],
-        [0, 1, 0, 1],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [1, 0, 0, 1],
         [0, 0, 0, 1]
     ])
     new_p1 = SE3(arr)
 
     arr = array([
-        [1, 0, 0, 0],
-        [0, 0, -1, 0],
-        [0, 1, 0, 2],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [1, 0, 0, 2],
         [0, 0, 0, 1]
     ])
     new_p2 = SE3(arr)
 
     arr = array([
-        [1, 0, 0, 0],
-        [0, 0, -1, 0],
-        [0, 1, 0, 3],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [1, 0, 0, 3],
         [0, 0, 0, 1]
     ])
     new_p3 = SE3(arr)
