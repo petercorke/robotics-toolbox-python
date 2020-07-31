eddff1c79401c2af3b46c0d8fc471bf86cce40c5
jhavl <jhavl@users.noreply.github.com>
2020-07-31 11:48:46 +1000

    Added manifest file

---
+++ .github/build-wheels.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e -u -x
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/wheelhouse/
+    fi
+}
+
+
+# Compile wheels
+for PYBIN in /opt/python/cp3[5-8]*/bin; do
+    "${PYBIN}/pip" install .
+    "${PYBIN}/pip" install pytest
+    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+# Install packages and test
+for PYBIN in /opt/python/cp3[5-8]*/bin/; do
+    "${PYBIN}/pip" install ropy --no-index -f /io/wheelhouse
+    ("${PYBIN}/pytest")
+done
