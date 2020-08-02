6550f6d222e0c80b341cbf756a887941f7513345
micah-huth <micah.huth@iinet.net.au>
2020-08-03 09:33:51 +1000

    Added functions to draw all available shapes for a 2D object.

--- graphics/graphics_object2d.py
+++ graphics/graphics_object2d.py
@@ -1,5 +1,5 @@
-from vpython import shapes, arrow, extrusion, vector
-
+from vpython import shapes, radians, extrusion, vector
+from spatialmath import SE2
 
 class Object2D:
     """
@@ -16,7 +16,7 @@ class Object2D:
     """
 
     # TODO
-    #  Have options to give stl file, shape, or arrow (default)
+    #  Have options to give stl file, shape, or arrow
     #  Have option to set size, colour, etc
     #  Have funcs that update pose, texture/colours, visibility
 
@@ -65,39 +65,42 @@ class Object2D:
             # 2D coords of the star boundary
             shape_path = shapes.star(radius=0.5, n=6)
         elif self.__shape == '.':
-            raise NotImplementedError()
-        elif self.__shape == 'x':
-            raise NotImplementedError()
-        elif self.__shape == 's':
             # 2D coords of the square boundary
             shape_path = shapes.rectangle(width=1, height=1)
+        elif self.__shape == 'x':
+            # 2D coords of the cross boundary
+            shape_path = shapes.cross(width=5, thickness=1, rotate=radians(45))
+        elif self.__shape == 's':
+            # 2D coords of the square boundary
+            shape_path = shapes.rectangle(width=1, height=1, thickness=0.1)
         elif self.__shape == 'd':
-            raise NotImplementedError()
+            # 2D coords of the diamond boundary
+            shape_path = shapes.rectangle(width=1, height=1, thickness=0.1, rotate=radians(45))
         elif self.__shape == '^':
             # 2D coords of the triangle boundary
             shape_path = shapes.triangle(length=1)
         elif self.__shape == 'v':
-            raise NotImplementedError()
+            # 2D coords of the triangle boundary
+            shape_path = shapes.triangle(length=1, rotate=radians(180))
         elif self.__shape == '<':
-            raise NotImplementedError()
+            # 2D coords of the triangle boundary
+            shape_path = shapes.triangle(length=1, rotate=radians(90))
         elif self.__shape == '>':
-            raise NotImplementedError()
+            # 2D coords of the triangle boundary
+            shape_path = shapes.triangle(length=1, rotate=radians(-90))
         elif self.__shape == 'p':
             # 2D coords of the pentagon boundary
-            shape_path =shapes.pentagon(length=1)
+            shape_path = shapes.pentagon(length=1)
         elif self.__shape == 'h':
             # 2D coords of the hexagon boundary
             shape_path = shapes.hexagon(length=1)
-
         # CURRENTLY UNUSED
         # elif self.__shape == 'o':
         #     # 2D coords of the octagon boundary
         #     shape_path = shapes.octagon(length=1)
         # elif self.__shape == 'r':
-        #     # 2D coords of the circle boundary (with thickness = 0.1*r)
+        #     # 2D coords of the ring boundary (with thickness = 10%)
         #     shape_path = shapes.circle(radius=0.5, thickness=0.1)
-
-
         else:
             raise ValueError("Invalid shape given")
 
