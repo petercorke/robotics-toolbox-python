e640067bca275ddd73b7cd3b413fc08542d4034f
jhavl <jhavl@users.noreply.github.com>
2020-08-06 11:29:15 +1000

    teach example finished

--- examples/teach.py
+++ examples/teach.py
@@ -26,7 +26,7 @@ def text_trans(text):
 
 def update(val, text):
     for i in range(panda.n):
-        panda.q[i] = sjoint[i].val
+        panda.q[i] = sjoint[i].val * np.pi/180
 
     text_trans(text)
     env.step(0)
@@ -48,38 +48,49 @@ text = []
 x1 = 0.04
 x2 = 0.22
 yh = 0.04
-ym = 0.5 - (panda.n * yh) / 2
+ym = 0.5 - (panda.n * yh) / 2 + 0.17/2
 
-axcolor = 'lightgoldenrodyellow'
 axjoint = []
 sjoint = []
 
-qlim = np.copy(panda.qlim)
+qlim = np.copy(panda.qlim) * 180/np.pi
 
 if not np.all(qlim != 0):
-    qlim[0, :] = -np.pi
-    qlim[1, :] = np.pi
+    qlim[0, :] = -180
+    qlim[1, :] = 180
 
 # Set the pose text
 T = panda.fkine()
 t = np.round(T.t, 3)
-r = np.round(T.rpy(), 3)
+r = np.round(T.rpy(unit='deg'), 3)
 
-text.append(fig.text(0.05, 0.8, "x: {0}".format(t[0]), fontsize=9))
-text.append(fig.text(0.05, 0.76, "y: {0}".format(t[1]), fontsize=9))
-text.append(fig.text(0.05, 0.72, "z: {0}".format(t[2]), fontsize=9))
-text.append(fig.text(0.17, 0.8, "r: {0}".format(r[0]), fontsize=9))
-text.append(fig.text(0.17, 0.76, "p: {0}".format(r[1]), fontsize=9))
-text.append(fig.text(0.17, 0.72, "y: {0}".format(r[2]), fontsize=9))
+fig.text(
+    0.02,  1 - ym + 0.25, "End-effector Pose",
+    fontsize=9, weight="bold", color="#4f4f4f")
+text.append(fig.text(
+    0.03, 1 - ym + 0.20, "x: {0}".format(t[0]), fontsize=9, color="#2b2b2b"))
+text.append(fig.text(
+    0.03, 1 - ym + 0.16, "y: {0}".format(t[1]), fontsize=9, color="#2b2b2b"))
+text.append(fig.text(
+    0.03, 1 - ym + 0.12, "z: {0}".format(t[2]), fontsize=9, color="#2b2b2b"))
+text.append(fig.text(
+    0.15, 1 - ym + 0.20, "r: {0}".format(r[0]), fontsize=9, color="#2b2b2b"))
+text.append(fig.text(
+    0.15, 1 - ym + 0.16, "p: {0}".format(r[1]), fontsize=9, color="#2b2b2b"))
+text.append(fig.text(
+    0.15, 1 - ym + 0.12, "y: {0}".format(r[2]), fontsize=9, color="#2b2b2b"))
+fig.text(
+    0.02,  1 - ym + 0.06, "Joint angles",
+    fontsize=9, weight="bold", color="#4f4f4f")
 
 for i in range(panda.n):
     ymin = (1 - ym) - i * yh
-    axjoint.append(plt.axes([x1, ymin, x2, 0.03], facecolor=axcolor))
+    axjoint.append(plt.axes([x1, ymin, x2, 0.03], facecolor='#dbdbdb'))
 
     sjoint.append(
         Slider(
             axjoint[i], 'q' + str(i),
-            qlim[0, i], qlim[1, i], panda.q[i]))
+            qlim[0, i], qlim[1, i], panda.q[i] * 180/np.pi))
 
     sjoint[i].on_changed(lambda x: update(x, text))
 
