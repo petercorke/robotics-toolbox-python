eba83d2dbe4ebf62070cfa37c67bd8202177e5d6
jhavl <jhavl@users.noreply.github.com>
2020-08-07 10:48:46 +1000

    Ellipse progress

--- examples/vellipse.py
+++ examples/vellipse.py
@@ -4,10 +4,40 @@
 """
 
 import ropy as rp
-# import numpy as np
+import numpy as np
+import spatialmath as sm
+from ropy.connect.PyPlot import Ellipse
+import time
 
-# Make a panda robot
+env = rp.PyPlot()
+env.launch('Panda Resolved-Rate Motion Control Example')
+
 panda = rp.PandaMDH()
-
-# Init joint to the 'ready' joint angles
 panda.q = panda.qr
+
+Tep = panda.fkine() * sm.SE3.Tx(-0.4) * sm.SE3.Tz(0.4) * sm.SE3.Rz(np.pi) 
+
+arrived = False
+env.add(panda)
+
+ell = Ellipse(env.ax)
+
+dt = 0.05
+
+while not arrived:
+    start = time.time()
+
+    A = panda.jacobe()[3:, 3:] @ np.transpose(panda.jacobe()[3:, 3:])
+    ell.make_ellipsoid(A, centre=panda.fkine().t)
+    ell.draw_ellipsoid()
+
+    v, arrived = rp.p_servo(panda.fkine(), Tep, 0.5)
+    panda.qd = np.linalg.pinv(panda.jacobe()) @ v
+    env.step(50)
+    stop = time.time()
+
+    if stop - start < dt:
+        print("Hello")
+        time.sleep(dt - (stop - start))
+
+env.hold()
--- ropy/connect/PyPlot.py
+++ ropy/connect/PyPlot.py
@@ -29,6 +29,42 @@ plt.rc('grid', linestyle="-", color='#dbdbdb')
 #         super(Ellipse, self).__init__()
 
 #         self.ax = ax
+#         self.ell = None
+
+#     def draw_ellipsoid(self):
+#         if self.ell is not None:
+#             self.ax.collections.remove(self.ell)
+
+#         self.ell = self.ax.plot_wireframe(
+#             self.x, self.y, self.z,  rstride=6,
+#             cstride=6, color='#2980b9', alpha=0.2)
+
+#     def make_ellipsoid(self, A, centre=[0, 0, 0]):
+#         """
+#         Plot the 3-d Ellipsoid ell on the Axes3D ax.
+
+#         """
+
+#         # find the rotation matrix and radii of the axes
+#         U, s, rotation = np.linalg.svd(A)
+#         radii = 1.0 / np.sqrt(s)
+
+#         # points on unit sphere
+#         u = np.linspace(0.0, 2.0 * np.pi, 60)
+#         v = np.linspace(0.0, np.pi, 60)
+#         x = radii[0] * np.outer(np.cos(u), np.sin(v))
+#         y = radii[1] * np.outer(np.sin(u), np.sin(v))
+#         z = radii[2] * np.outer(np.ones_like(u), np.cos(v))
+
+#         # transform points to ellipsoid
+#         for i in range(len(x)):
+#             for j in range(len(x)):
+#                 [x[i, j], y[i, j], z[i, j]] = \
+#                     np.dot([x[i, j], y[i, j], z[i, j]], rotation) + centre * 10
+
+#         self.x = x / 10
+#         self.y = y / 10
+#         self.z = z / 10
 
 
 class RobotPlot(object):
