d1464d85d9127a002bb436d47f263135e34bbdda
jhavl <jhavl@users.noreply.github.com>
2020-08-14 16:54:36 +1000

    Panda model updated

--- ropy/models/Panda.py
+++ ropy/models/Panda.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
-from spatialmath.base import trotz, transl
 from ropy.robot.ETS import ETS
 from ropy.robot.ET import ET
+from ropy.robot.ELink import ELink
 
 
 class Panda(ETS):
@@ -20,6 +20,7 @@ class Panda(ETS):
     :references:
         - Kinematic Derivatives using the Elementary Transform
           Sequence, J. Haviland and P. Corke
+
     """
     def __init__(self):
 
@@ -27,36 +28,60 @@ class Panda(ETS):
         mm = 1e-3
         tool_offset = (103)*mm
 
-        et_list = [
-            ET.Ttz(0.333),
-            ET.TRz(),
-            ET.TRx(-90*deg),
-            ET.TRz(),
-            ET.TRx(90*deg),
-            ET.Ttz(0.316),
-            ET.TRz(),
-            ET.Ttx(0.0825),
-            ET.TRx(90*deg),
-            ET.TRz(),
-            ET.Ttx(-0.0825),
-            ET.TRx(-90*deg),
-            ET.Ttz(0.384),
-            ET.TRz(),
-            ET.TRx(90*deg),
-            ET.TRz(),
-            ET.Ttx(0.088),
-            ET.TRx(90*deg),
-            ET.Ttz(0.107),
-            ET.TRz(),
-        ]
+        l0 = ELink(
+            [ET.Ttz(0.333), ET.TRz()],
+            name='link0',
+            parent=None
+        )
 
-        tool = transl(0, 0, tool_offset) @  trotz(-np.pi/4)
+        l1 = ELink(
+            [ET.TRx(-90*deg), ET.TRz()],
+            name='link1',
+            parent=l0
+        )
+
+        l2 = ELink(
+            [ET.TRx(90*deg), ET.Ttz(0.316), ET.TRz()],
+            name='link2',
+            parent=l1
+        )
+
+        l3 = ELink(
+            [ET.Ttx(0.0825), ET.TRx(90*deg), ET.TRz()],
+            name='link3',
+            parent=l2
+        )
+
+        l4 = ELink(
+            [ET.Ttx(-0.0825), ET.TRx(-90*deg), ET.Ttz(0.384), ET.TRz()],
+            name='link4',
+            parent=l3
+        )
+
+        l5 = ELink(
+            [ET.TRx(90*deg), ET.TRz()],
+            name='link5',
+            parent=l4
+        )
+
+        l6 = ELink(
+            [ET.Ttx(0.088), ET.TRx(90*deg), ET.Ttz(0.107), ET.TRz()],
+            name='link6',
+            parent=l5
+        )
+
+        ee = ELink(
+            [ET.Ttz(tool_offset), ET.TRz(-np.pi/4)],
+            name='ee',
+            parent=l6
+        )
+
+        ETlist = [l0, l1, l2, l3, l4, l5, l6, ee]
 
         super(Panda, self).__init__(
-            et_list,
+            ETlist,
             name='Panda',
-            manufacturer='Franka Emika',
-            tool=tool)
+            manufacturer='Franka Emika')
 
         self._qz = np.array([0, 0, 0, 0, 0, 0, 0])
         self._qr = np.array([0, -0.3, 0, -2.2, 0, 2.0, np.pi/4])
