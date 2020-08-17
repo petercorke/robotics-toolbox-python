ff61799b5721c0787bd72b0be02ec7163e6a717c
jhavl <jhavl@users.noreply.github.com>
2020-08-17 14:51:44 +1000

    Shape primitives added

--- ropy/robot/Shape.py
+++ ropy/robot/Shape.py
@@ -5,74 +5,54 @@
 
 import numpy as np
 from spatialmath import SE3
-from spatialmath.base.argcheck import getvector, verifymatrix, isscalar
-import ropy as rp
+from spatialmath.base.argcheck import getvector
 
 
 class Shape(object):
 
-    def __init__(self):
+    def __init__(
+            self,
+            primitive,
+            base=None,
+            radius=0,
+            length=0,
+            scale=[1, 1, 1],
+            filename=None):
 
-        primitive = True
-
-
-
-class Mesh(Shape):
-    """
-    A triangular mesh object.
-
-    :param filename: The path to the mesh that contains this object.
-        This is the absolute path.
-    :type filename: str
-        
-    :param scale: The scaling value for the mesh along the XYZ axes. If
-        ``None``, assumes no scale is applied.
-    :type scale: list (3) float, optional
-
-    """
-
-    def __init__(self, filename, scale=None):
-        self.filename = filename
+        self.base = base
+        self.primitive = primitive
         self.scale = scale
+        self.filename = filename
 
     @property
-    def filename(self):
-        """str : The path to the mesh file for this object.
-        """
-        return self._filename
+    def base(self):
+        return self._base
 
-    @filename.setter
-    def filename(self, value):
-        self._filename = value
+    @property
+    def primitive(self):
+        return self._primitive
+
+    @primitive.setter
+    def primitive(self, value):
+        self._primitive = value
 
     @property
     def scale(self):
-        """(3,) float : A scaling for the mesh along its local XYZ axes.
-        """
         return self._scale
 
     @scale.setter
     def scale(self, value):
         if value is not None:
-            value = np.asanyarray(value).astype(np.float64)
+            value = getvector(value, 3)
         self._scale = value
 
+    @property
+    def filename(self):
+        return self._filename
 
-class Cylinder(Shape):
-    """A cylinder whose center is at the local origin.
-    Parameters
-    ----------
-    :param radius: The radius of the cylinder in meters.
-    :type radius: float
-        The radius of the cylinder in meters.
-    :param length: The length of the cylinder in meters.
-    :type length: float
-
-    """
-
-    def __init__(self, radius, length):
-        self.radius = radius
-        self.length = length
+    @filename.setter
+    def filename(self, value):
+        self._filename = value
 
     @property
     def radius(self):
@@ -90,6 +70,48 @@ class Cylinder(Shape):
     def length(self, value):
         self._length = float(value)
 
+    @base.setter
+    def base(self, T):
+        if not isinstance(T, SE3):
+            T = SE3(T)
+        self._base = T
+
+
+class Mesh(Shape):
+    """
+    A mesh object.
+
+    :param filename: The path to the mesh that contains this object.
+        This is the absolute path.
+    :type filename: str
+    :param scale: The scaling value for the mesh along the XYZ axes. If
+        ``None``, assumes no scale is applied.
+    :type scale: list (3) float, optional
+
+    """
+
+    def __init__(self, filename, base=None, scale=None):
+        super(Box, self).__init__(
+            False, filename=filename, base=base, scale=scale)
+
+
+class Cylinder(Shape):
+    """A cylinder whose center is at the local origin.
+    Parameters
+    ----------
+    :param radius: The radius of the cylinder in meters.
+    :type radius: float
+        The radius of the cylinder in meters.
+    :param length: The length of the cylinder in meters.
+    :type length: float
+
+    """
+
+    def __init__(self, radius, length, base=None):
+        super(Box, self).__init__(
+            
+            True, base=base, radius=radius, length=length)
+
 
 class Sphere(Shape):
     """
@@ -100,13 +122,19 @@ class Sphere(Shape):
 
     """
 
-    def __init__(self, radius):
-        self.radius = radius
+    def __init__(self, radius, base=None):
+        super(Box, self).__init__(True, base=base, radius=radius)
 
-    @property
-    def radius(self):
-        return self._radius
 
-    @radius.setter
-    def radius(self, value):
-        self._radius = float(value)
+class Box(Shape):
+    """
+    A rectangular prism whose center is at the local origin.
+
+    :param scale: The length, width, and height of the box in meters.
+    :type scale: list (3) float
+
+    """
+
+    def __init__(self, scale, base=None):
+
+        super(Box, self).__init__(True, base=base, scale=scale)
