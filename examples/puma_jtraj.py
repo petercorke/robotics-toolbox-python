2b36075b0eb10a5a81a19dcfc555cebbba51c892
Peter Corke <peter.i.corke@gmail.com>
2020-09-13 17:04:20 +1000

    add simple examples

---
+++ examples/puma_jtraj.py
@@ -0,0 +1,10 @@
+import roboticstoolbox as rtb
+from spatialmath import *
+
+
+p560 = rtb.models.DH.Puma560()
+print(p560)
+tw, T0 = p560.twists(p560.qn)
+print(tw)
+qt = rtb.tools.trajectory.jtraj(p560.qr, p560.qz, 50)
+p560.plot(qt.q.T, dt=100, movie='puma_sitting.gif')
