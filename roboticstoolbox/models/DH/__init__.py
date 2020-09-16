ea95f26bb213f6ac6337a37399434fd50019e8d1
Peter Corke <peter.i.corke@gmail.com>
2020-09-16 15:05:56 +1000

    add new robots to the package

--- roboticstoolbox/models/DH/__init__.py
+++ roboticstoolbox/models/DH/__init__.py
@@ -1,10 +1,20 @@
 from roboticstoolbox.models.DH.Panda import Panda
 from roboticstoolbox.models.DH.Puma560 import Puma560
 from roboticstoolbox.models.DH.Stanford import Stanford
+from roboticstoolbox.models.DH.Ball import Ball
+from roboticstoolbox.models.DH.Cobra600 import Cobra600
+from roboticstoolbox.models.DH.IRB140 import IRB140
+from roboticstoolbox.models.DH.KR5 import KR5
+from roboticstoolbox.models.DH.Threelink import Threelink
 
 
 __all__ = [
     'Panda',
     'Puma560',
-    'Stanford'
+    'Stanford',
+    'Ball',
+    'Cobra600',
+    'IRB140',
+    'KR5',
+    'Threelink'
 ]
