eeafc3e6813d452a3a5f33174b87ae74279cf0d6
micah-huth <micah.huth@iinet.net.au>
2020-10-05 14:32:57 +1000

    Created example of using VPython backend

---
+++ examples/VPython.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+@author Micah Huth
+"""
+
+import roboticstoolbox as rp
+import time
+
+env = rp.backend.VPython()
+env.launch()
+
+puma = rp.models.Puma560()
+env.add(0, 'Puma', puma)
+
+time.sleep(1)
+env.step(puma, puma.qz, 0)
+
+time.sleep(1)
+env.step(puma, puma.qr, 0)
+
+time.sleep(1)
+env.step(puma, puma.qs, 0)
+
+time.sleep(1)
+env.step(puma, puma.qn, 0)
