aa26c45f2dea48da33b50450739e48bb6678440a
micah-huth <micah.huth@iinet.net.au>
2020-10-09 12:47:10 +1000

    Rearrange pose order

--- examples/VPython.py
+++ examples/VPython.py
@@ -13,9 +13,6 @@ puma = rp.models.DH.Puma560()
 env.add(0, 'Puma', puma)
 
 time.sleep(1)
-env.step(puma, puma.qz, 0)
-
-time.sleep(1)
 env.step(puma, puma.qr, 0)
 
 time.sleep(1)
@@ -23,3 +20,6 @@ env.step(puma, puma.qs, 0)
 
 time.sleep(1)
 env.step(puma, puma.qn, 0)
+
+time.sleep(1)
+env.step(puma, puma.qz, 0)
