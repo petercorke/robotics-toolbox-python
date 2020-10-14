10cdeb4034cb8d3b938ed114e5b8b090bd0372d3
micah-huth <micah.huth@iinet.net.au>
2020-10-14 13:36:42 +1000

    Updated sleep time

--- examples/VPython.py
+++ examples/VPython.py
@@ -12,14 +12,14 @@ env.launch()
 puma = rp.models.DH.Puma560()
 env.add(0, 'Puma', puma)
 
-time.sleep(1)
+time.sleep(3)
 env.step(puma, puma.qr, 0)
 
-time.sleep(1)
+time.sleep(3)
 env.step(puma, puma.qs, 0)
 
-time.sleep(1)
+time.sleep(3)
 env.step(puma, puma.qn, 0)
 
-time.sleep(1)
+time.sleep(3)
 env.step(puma, puma.qz, 0)
