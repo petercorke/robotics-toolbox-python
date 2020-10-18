18ba22314178aa95346292abf5a0d5b8d472ccd2
Peter Corke <peter.i.corke@gmail.com>
2020-10-18 15:37:43 +1000

    Port of jsingu working

---
+++ roboticstoolbox/tools/jsingu.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.linalg import lu
+
+
+def jsingu(J):
+
+    # convert to row-echelon form
+    P, L, U = lu(J)
+    U = np.where(abs(U) < 100 * np.finfo(np.float64).eps, False, True)
+
+    for j in range(J.shape[0]):
+        if U[j,j] == False:
+            print(f'joint {j} is dependent on joint ' + 
+                    ', '.join([str(i) for i in range(j) if all(U[:,j] == U[:,i])]))
+
+if __name__ == "__main__":
+    import roboticstoolbox as rtb
+
+    puma = rtb.models.DH.Puma560()
+    J = puma.jacob0(puma.qr)
+
+    jsingu(J)
+
