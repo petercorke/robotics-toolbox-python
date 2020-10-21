50aea60acaff88a86242f2be139490e8025cd992
micah-huth <micah.huth@iinet.net.au>
2020-10-21 16:06:37 +1000

    Commented out stdout_supress usage

--- roboticstoolbox/tools/__init__.py
+++ roboticstoolbox/tools/__init__.py
@@ -3,7 +3,7 @@
 from roboticstoolbox.tools.null import null
 from roboticstoolbox.tools.p_servo import p_servo
 from roboticstoolbox.tools.ticker import Ticker
-from roboticstoolbox.tools.stdout_supress import stdout_supress
+# from roboticstoolbox.tools.stdout_supress import stdout_supress
 
 __all__ = [
     # 'is_vector',
@@ -20,5 +20,5 @@ __all__ = [
     'null',
     'p_servo',
     'trajectory',
-    'stdout_supress'
+    # 'stdout_supress'
 ]
