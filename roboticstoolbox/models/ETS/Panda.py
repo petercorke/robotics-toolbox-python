e079f9a9351a8a9082f0b934a144c58d4170218d
Samuel-Drew <62736662+Samuel-Drew@users.noreply.github.com>
2020-10-28 12:18:42 +1000

    Revert "Revert "Merge branch 'master' of https://github.com/petercorke/robotics-toolbox-python""
    This reverts commit fbef42f2bbb799c92d0f5d58dd4807d38c4de950.

--- roboticstoolbox/models/ETS/Panda.py
+++ roboticstoolbox/models/ETS/Panda.py
@@ -29,50 +29,43 @@ class Panda(ERobot):
         tool_offset = (103)*mm
 
         l0 = ELink(
-            ETS.tz(0.333),
-            ETS.rz(),
+            ETS.tz(0.333) * ETS.rz(),
             name='link0',
             parent=None
         )
 
         l1 = ELink(
-            ETS.rx(-90*deg),
-            ETS.rz(),
+            ETS.rx(-90*deg) * ETS.rz(),
             name='link1',
             parent=l0
         )
 
         l2 = ELink(
-            ETS.rx(90*deg) * ETS.tz(0.316),
-            ETS.rz(),
+            ETS.rx(90*deg) * ETS.tz(0.316) * ETS.rz(),
             name='link2',
             parent=l1
         )
 
         l3 = ELink(
-            ETS.tx(0.0825) * ETS.rx(90*deg),
-            ETS.rz(),
+            ETS.tx(0.0825) * ETS.rx(90, 'deg') * ETS.rz(),
             name='link3',
             parent=l2
         )
 
         l4 = ELink(
-            ETS.tx(-0.0825) * ETS.rx(-90*deg) * ETS.tz(0.384),
-            ETS.rz(),
+            ETS.tx(-0.0825) * ETS.rx(-90, 'deg') * ETS.tz(0.384) * ETS.rz(),
             name='link4',
             parent=l3
         )
 
         l5 = ELink(
-            ETS.rx(90*deg),
-            ETS.rz(),
+            ETS.rx(90, 'deg') * ETS.rz(),
             name='link5',
             parent=l4
         )
 
         l6 = ELink(
-            ETS.tx(0.088) * ETS.rx(90*deg) * ETS.tz(0.107),
-            ETS.rz(),
+            ETS.tx(0.088) * ETS.rx(90, 'deg') * ETS.tz(0.107) * ETS.rz(),
             name='link6',
             parent=l5
         )
@@ -100,3 +93,32 @@ if __name__ == '__main__':   # pragma nocover
 
     robot = Panda()
     print(robot)
+
+    print(robot.n, robot.M)
+    print(robot.elinks)
+    print(robot.q_idx)
+    for link in robot:
+        print(link.name, link)
+        print(link.ets, link.v, link.isjoint)
+        print(link.parent, link.child)
+        print(link.Ts)
+        print(link.geometry, link.collision)
+
+        print()
+
+    from ansitable import ANSITable, Column
+
+    table = ANSITable(
+        Column("link"),
+        Column("parent"),
+        Column("ETS", headalign="^", colalign="<"),
+        border="thin")
+    for link in robot:
+        if link.isjoint:
+            color = ""
+        else:
+            color = "<<blue>>"
+        table.row(color + link.name, 
+            link.parent.name if link.parent is not None else "-", 
+            link.ets * link.v if link.v is not None else link.ets)
+    table.print()
