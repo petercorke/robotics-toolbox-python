46292554a0739f0482db7474a75e3f3bee19657c
Peter Corke <peter.i.corke@gmail.com>
2020-11-09 07:19:39 +1000

    make into proper class

--- roboticstoolbox/models/ETS/__init__.py
+++ roboticstoolbox/models/ETS/__init__.py
@@ -1,7 +1,10 @@
 from roboticstoolbox.models.ETS.Panda import Panda
 from roboticstoolbox.models.ETS.Frankie import Frankie
+from roboticstoolbox.models.ETS.Puma560 import Puma560
+
 
 __all__ = [
     'Panda',
-    'Frankie'
+    'Frankie',
+    'Puma560',
 ]
