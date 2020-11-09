mass fixed and p_servo radians

roboticstoolbox/backend/urdf/urdf.py
@@ -1531,7 +1531,7 @@ class Link(URDFType):
             raise TypeError('Expected Inertial object')   # pragma nocover
         # Set default inertial
         if value is None:
-            value = Inertial(mass=1.0, inertia=np.eye(3))
+            value = Inertial(mass=0.0, inertia=np.eye(3))
         self._inertial = value
 
     @property

roboticstoolbox/tools/p_servo.py
@@ -43,7 +43,7 @@ def p_servo(wTe, wTep, gain=2, threshold=0.1):
     ev = eTep.t
 
     # Angular velocity error
-    ew = eTep.rpy() * np.pi/180
+    ew = eTep.rpy('rad')
 
     # Form error vector
     e = np.r_[ev, ew]