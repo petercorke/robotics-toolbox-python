8bb778ebdea9ef6d3e70009771072c3ce3f2697b
Peter Corke <peter.i.corke@gmail.com>
2020-11-16 18:45:52 +1000

    tidyup remove cruft comment out Panda code

--- examples/puma_jtraj.py
+++ examples/puma_jtraj.py
@@ -2,9 +2,17 @@ import roboticstoolbox as rtb
 from spatialmath import *
 
 
-p560 = rtb.models.DH.Puma560()
-print(p560)
-tw, T0 = p560.twists(p560.qn)
-print(tw)
-qt = rtb.tools.trajectory.jtraj(p560.qr, p560.qz, 50)
-p560.plot(qt.q.T, dt=100, movie='puma_sitting.gif')
+#robot = rtb.models.URDF.Puma560()
+robot = rtb.models.DH.Puma560()
+print(robot)
+print(robot.fkine(robot.qz))
+# tw, T0 = p560.twists(p560.qn)
+# print(tw)
+qt = rtb.tools.trajectory.jtraj(robot.qz, robot.qr, 200)
+robot.plot(qt.q.T, dt=100, block=True) #movie='puma_sitting.gif')
+#swift = rtb.backend.Swift()
+#swift.launch()
+#swift.add(robot)
+#for q in qt.q:
+	#robot.q = q
+	#swift.step()
