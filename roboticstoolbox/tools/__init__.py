c29af8e427ab14962a192d27dc02930ed593daaf
jhavl <jhavl@users.noreply.github.com>
2020-11-16 12:06:56 +1000

    jsingu tests

--- roboticstoolbox/tools/__init__.py
+++ roboticstoolbox/tools/__init__.py
@@ -1,14 +1,14 @@
 from roboticstoolbox.tools.null import null
 from roboticstoolbox.tools.p_servo import p_servo
 from roboticstoolbox.tools.Ticker import Ticker
-from roboticstoolbox.tools.urdf import *
+from roboticstoolbox.tools.urdf import *  # noqa
 from roboticstoolbox.tools.trajectory import tpoly, \
     jtraj, ctraj, lspb, t1plot, qplot, mstraj
+from roboticstoolbox.tools.jsingu import jsingu
 
 __all__ = [
     'null',
     'p_servo',
-    'trajectory',
     'Ticker',
     'tpoly',
     'jtraj',
@@ -16,5 +16,6 @@ __all__ = [
     'lspb',
     't1plot',
     'qplot',
-    'mstraj'
+    'mstraj',
+    'jsingu'
 ]
