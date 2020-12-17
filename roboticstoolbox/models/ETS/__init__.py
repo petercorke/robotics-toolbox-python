6f97e57cd27e8d913fd3ee1daf3f79512b8b4124
Peter Corke <peter.i.corke@gmail.com>
2020-12-17 22:01:35 +1000

    add planar_Y

--- roboticstoolbox/models/ETS/__init__.py
+++ roboticstoolbox/models/ETS/__init__.py
@@ -1,10 +1,12 @@
 from roboticstoolbox.models.ETS.Panda import Panda
 from roboticstoolbox.models.ETS.Frankie import Frankie
 from roboticstoolbox.models.ETS.Puma560 import Puma560
+from roboticstoolbox.models.ETS.Planar_Y import Planar_Y
 
 
 __all__ = [
     'Panda',
     'Frankie',
     'Puma560',
+    'Planar_Y',
 ]
