9958ba7266414cb5569c2604b96fb85a9fcf8213
Peter Corke <peter.i.corke@gmail.com>
2021-03-07 10:38:41 +1000

    vpython examples

---
+++ examples/vpython-teach.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+@author Micah Huth
+"""
+
+import roboticstoolbox as rtb
+import time
+
+from roboticstoolbox.backends.VPython import VPython
+
+env = VPython()
+env.launch()
+
+
+#  PUMA560
+puma = rtb.models.DH.Puma560()
+
+
+env.hold()
