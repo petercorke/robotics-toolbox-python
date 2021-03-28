373648cc0dfaedba212de947366c346b53f330a5
Peter Corke <peter.i.corke@gmail.com>
2021-03-28 18:43:52 +1000

    Add Planar2 model

---
+++ roboticstoolbox/models/ETS/Planar2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import numpy as np
+from roboticstoolbox.robot.ETS import ETS2
+from roboticstoolbox.robot.ERobot import ERobot
+from roboticstoolbox.robot.ELink import ELink
+
+
+class Planar2(ERobot):
+    """
+    Create model of a branched planar manipulator::
+
+        L0 -- L1 -+- L2a -- L3a -- EEa
+                |
+                +- L2b -- L3b -- EEb
+
+    ``Planar_Y()`` creates a planar branched manipulator model.
+
+
+    :references:
+        - Kinematic Derivatives using the Elementary Transform
+          Sequence, J. Haviland and P. Corke
+
+    """
+    def __init__(self):
+
+        a1 = 1
+        a2 = 1
+
+        # ets = ETS2.r() * ETS2.tx(a1) * ETS2.r() * ETS2.tx(a2)
+
+        l0 = ELink(ETS2.r(), name='link0')
+        l1 = ELink(ETS2.tx(a1) * ETS2.r(), name='link1', parent=l0)
+        l2 = ELink(ETS2.tx(a2), name='ee', parent=l1)
+
+        super().__init__(
+            [l0, l1, l2],
+            name='Planar2',
+            comment='Planar 2D manipulator')
+
+        self.addconfiguration(
+            "qz", [0, 0])
+
+
+if __name__ == '__main__':   # pragma nocover
+
+    robot = Planar2()
+    print(robot)
+
+    print(robot.fkine(robot.qz))
+
