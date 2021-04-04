e1bbd5ba8c70eb3a28b90406b7a53f9a9a551306
jhavl <jhavl@users.noreply.github.com>
2021-04-04 14:10:47 +1000

    fixed some fknm bugs

--- roboticstoolbox/robot/Robot.py
+++ roboticstoolbox/robot/Robot.py
@@ -74,22 +74,10 @@ class Robot(BaseRobot, DynamicsMixin, IKMixin):
         if not isinstance(links, list):
             raise TypeError('The links must be stored in a list.')
 
-        self._hasdynamics = False
-        self._hasgeometry = False
-
         for link in links:
             if not isinstance(link, Link):
                 raise TypeError('links should all be Link subclass')
-
-            # add link back to roboto
             link._robot = self
-
-            if link._hasdynamics:
-                self._hasdynamics = True
-
-            if isinstance(link, rtb.ELink):
-                if len(link.geometry) > 0:
-                    self._hasgeometry = True
         self._links = links
 
         # Current joint angles of the robot
@@ -101,7 +89,7 @@ class Robot(BaseRobot, DynamicsMixin, IKMixin):
 
         self._configdict = {}
 
-        self._dynchanged = False
+        self._dynchanged = True
 
         # URDF Parser Attempt
         # # Search mesh dir for meshes
@@ -163,7 +151,7 @@ class Robot(BaseRobot, DynamicsMixin, IKMixin):
     #
     #     return data
 
-    def dynchanged(self, what=None):
+    def dynchanged(self):
         """
         Dynamic parameters have changed (Robot superclass)
 
@@ -173,8 +161,6 @@ class Robot(BaseRobot, DynamicsMixin, IKMixin):
         :seealso: :func:`roboticstoolbox.Link._listen_dyn`
         """
         self._dynchanged = True
-        if what != 'gravity':
-            self._hasdynamics = True
 
     def _getq(self, q=None):
         """
