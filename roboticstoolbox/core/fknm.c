be2bbead3ffc81dd998d60b928ee0af3dd1a495c
jhavl <jhavl@users.noreply.github.com>
2021-05-31 10:24:19 +1000

    Fix free inv

--- roboticstoolbox/core/fknm.c
+++ roboticstoolbox/core/fknm.c
@@ -1115,6 +1115,7 @@ int _inv(npy_float64 *m, npy_float64 *invOut)
     det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
 
     if (det == 0)
+        free(inv);
         return 0;
 
     det = 1.0 / det;
