053f9b5785534dee24d68b6c183be06867de4a13
jhavl <jhavl@users.noreply.github.com>
2021-08-10 09:14:07 +1000

    fixed example

--- roboticstoolbox/examples/mexican-wave.py
+++ roboticstoolbox/examples/mexican-wave.py
@@ -1,4 +1,10 @@
-# a circle of Puma robot's doing a Mexican wave
+#!/usr/bin/env python
+"""
+@author Peter Corke
+@author Jesse Haviland
+"""
+
+# A circle of Puma robot's doing a Mexican wave
 
 import numpy as np
 from spatialmath import SE3
@@ -6,34 +12,41 @@ import roboticstoolbox as rtb
 from roboticstoolbox.backends.swift import Swift
 import time
 
-puma0 = rtb.models.URDF.Puma560()
 swift = Swift()
-
 swift.launch()
 
+puma0 = rtb.models.URDF.Puma560()
 pumas = []
+num_robots = 15
+rotation = 2 * np.pi * ((num_robots - 1) / num_robots)
 
-for theta in np.linspace(0, 2 * np.pi, 10):
+for theta in np.linspace(0, rotation, num_robots):
     base = SE3.Rz(theta) * SE3(2, 0, 0)
 
-    puma = rtb.ERobot(puma0)  # clone the robot
+    # Clone the robot
+    puma = rtb.ERobot(puma0)
     puma.base = base
     puma.q = puma0.qz
     swift.add(puma)
     pumas.append(puma)
 
-# the wave is a Gaussian that moves around the circle
-tt = np.linspace(0, 10, 100)
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
-g = gaussian(tt, 5, 1)
+# The wave is a Gaussian that moves around the circle
+tt = np.linspace(0, num_robots, num_robots * 10)
 
-for t in range(1000):
+
+def gaussian(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.0) / (2 * np.power(sig, 2.0)))
+
+
+g = gaussian(tt, 5, 1)
+t = 0
+
+while True:
     for i, puma in enumerate(pumas):
         k = (t + i * 10) % len(tt)
         puma.q = np.r_[0, g[k], -g[k], 0, 0, 0]
 
-        # NEXT LINE CAUSES 9/10 ROBOTS TO DISAPPEAR 
-        swift.step(0.1)
-        time.sleep(0.02)
-    
+        swift.step(0)
+        time.sleep(0.001)
+
+    t += 1
