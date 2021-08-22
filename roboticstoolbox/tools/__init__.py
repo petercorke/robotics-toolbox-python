371e0872299591169131f2f29b4b9335c18bbaa1
jhavl <jhavl@users.noreply.github.com>
2021-08-23 07:02:58 +1000

    include old p_servo

--- roboticstoolbox/tools/__init__.py
+++ roboticstoolbox/tools/__init__.py
@@ -1,32 +1,41 @@
 from roboticstoolbox.tools.null import null
-from roboticstoolbox.tools.p_servo import p_servo
+from roboticstoolbox.tools.p_servo import p_servo, p_servo_old
 from roboticstoolbox.tools.Ticker import Ticker
 from roboticstoolbox.tools.urdf import *  # noqa
-from roboticstoolbox.tools.trajectory import tpoly, tpoly_func, \
-    jtraj, mtraj, ctraj, lspb, lspb_func, qplot, mstraj
-from roboticstoolbox.tools.numerical import jacobian_numerical, \
-    hessian_numerical
+from roboticstoolbox.tools.trajectory import (
+    tpoly,
+    tpoly_func,
+    jtraj,
+    mtraj,
+    ctraj,
+    lspb,
+    lspb_func,
+    qplot,
+    mstraj,
+)
+from roboticstoolbox.tools.numerical import jacobian_numerical, hessian_numerical
 from roboticstoolbox.tools.jsingu import jsingu
 from roboticstoolbox.tools.data import rtb_loaddata, rtb_loadmat, rtb_path_to_datafile
 
 
 __all__ = [
-    'null',
-    'p_servo',
-    'Ticker',
-    'tpoly',
-    'tpoly_func',
-    'jtraj',
-    'ctraj',
-    'lspb',
-    'lspb_func',
-    'qplot',
-    'mtraj',
-    'mstraj',
-    'jsingu',
-    'jacobian_numerical',
-    'hessian_numerical',
-    'rtb_loaddata',
-    'rtb_loadmat',
-    'rtb_path_to_datafile',
+    "null",
+    "p_servo",
+    "p_servo_old",
+    "Ticker",
+    "tpoly",
+    "tpoly_func",
+    "jtraj",
+    "ctraj",
+    "lspb",
+    "lspb_func",
+    "qplot",
+    "mtraj",
+    "mstraj",
+    "jsingu",
+    "jacobian_numerical",
+    "hessian_numerical",
+    "rtb_loaddata",
+    "rtb_loadmat",
+    "rtb_path_to_datafile",
 ]
