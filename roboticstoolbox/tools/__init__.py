9f548831cef9ab76d778246555dfbd77fda666de
jhavl <jhavl@users.noreply.github.com>
2021-09-13 11:17:12 +1000

    Finish p_servo and tests

--- roboticstoolbox/tools/__init__.py
+++ roboticstoolbox/tools/__init__.py
@@ -1,5 +1,5 @@
 from roboticstoolbox.tools.null import null
-from roboticstoolbox.tools.p_servo import p_servo, p_servo_old
+from roboticstoolbox.tools.p_servo import p_servo
 from roboticstoolbox.tools.Ticker import Ticker
 from roboticstoolbox.tools.urdf import *  # noqa
 from roboticstoolbox.tools.trajectory import (
@@ -21,7 +21,6 @@ from roboticstoolbox.tools.data import rtb_loaddata, rtb_loadmat, rtb_path_to_da
 __all__ = [
     "null",
     "p_servo",
-    "p_servo_old",
     "Ticker",
     "tpoly",
     "tpoly_func",
--- roboticstoolbox/tools/p_servo.py
+++ roboticstoolbox/tools/p_servo.py
@@ -5,65 +5,10 @@ from spatialmath import SE3, base
 import math
 
 
-def p_servo_old(wTe, wTep, gain=2, threshold=0.1):
-    """
-    Position-based servoing.
-
-    Returns the end-effector velocity which will cause the robot to approach
-    the desired pose.
-
-    :param wTe: The current pose of the end-effecor in the base frame.
-    :type wTe: SE3
-    :param wTep: The desired pose of the end-effecor in the base frame.
-    :type wTep: SE3
-    :param gain: The gain for the controller
-    :type gain: float
-    :param threshold: The threshold or tolerance of the final error between
-        the robot's pose and desired pose
-    :type threshold: float
-
-    :returns v: The velocity of the end-effecotr which will casue the robot
-        to approach wTep
-    :rtype v: ndarray(6)
-    :returns arrived: True if the robot is within the threshold of the final
-        pose
-    :rtype arrived: bool
-
-    """
-
-    if not isinstance(wTe, SE3):
-        wTe = SE3(wTe)
-
-    if not isinstance(wTep, SE3):
-        wTep = SE3(wTep)
-
-    # Pose difference
-    eTep = wTe.inv() * wTep
-
-    # Translational error
-    ev = eTep.t
-
-    # Angular error
-    ew = eTep.rpy("rad")
-
-    # Form error vector
-    e = np.r_[ev, ew]
-
-    # Desired end-effector velocity
-    v = gain * e
-
-    if np.sum(np.abs(e)) < threshold:
-        arrived = True
-    else:
-        arrived = False
-
-    return v, arrived
-
-
 def _angle_axis(T, Td):
-    e = np.zeros(6)
-    e[:3] = Td.t - T.t
-    R = Td.R @ T.R.T
+    e = np.empty(6)
+    e[:3] = Td[:3, -1] - T[:3, -1]
+    R = Td[:3, :3] @ T[:3, :3].T
     li = np.array([R[2, 1] - R[1, 2], R[0, 2] - R[2, 0], R[1, 0] - R[0, 1]])
 
     if base.iszerovec(li):
@@ -83,19 +28,62 @@ def _angle_axis(T, Td):
     return e
 
 
-def p_servo(wTe, wTep, gain=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0], threshold=0.1):
-    if not isinstance(wTe, SE3):
-        wTe = SE3(wTe)
+def p_servo(wTe, wTep, gain=1.0, threshold=0.1, method="rpy"):
+    """
+    Position-based servoing.
 
-    if not isinstance(wTep, SE3):
-        wTep = SE3(wTep)
+    Returns the end-effector velocity which will cause the robot to approach
+    the desired pose.
 
-    k = np.diag(gain)
+    :param wTe: The current pose of the end-effecor in the base frame.
+    :type wTe: SE3 or ndarray
+    :param wTep: The desired pose of the end-effecor in the base frame.
+    :type wTep: SE3 or ndarray
+    :param gain: The gain for the controller. Can be vector corresponding to each
+        axis, or scalar corresponding to all axes.
+    :type gain: float, or array-like
+    :param threshold: The threshold or tolerance of the final error between
+        the robot's pose and desired pose
+    :type threshold: float
+    :param method: The method used to calculate the error. Default is 'rpy' -
+        error in the end-effector frame. 'angle-axis' - error in the base frame
+        using angle-axis method.
+    :type method: string: 'rpy' or 'angle-axis'
 
-    e = _angle_axis(wTe, wTep)
+    :returns v: The velocity of the end-effecotr which will casue the robot
+        to approach wTep
+    :rtype v: ndarray(6)
+    :returns arrived: True if the robot is within the threshold of the final
+        pose
+    :rtype arrived: bool
+
+    """
+
+    if isinstance(wTe, SE3):
+        wTe = wTe.A
+
+    if isinstance(wTep, SE3):
+        wTep = wTep.A
+
+    if method == "rpy":
+        # Pose difference
+        eTep = np.linalg.inv(wTe) @ wTep
+        e = np.empty(6)
+
+        # Translational error
+        e[:3] = eTep[:3, -1]
+
+        # Angular error
+        e[3:] = base.tr2rpy(eTep, unit="rad", order="zyx", check=False)
+    else:
+        e = _angle_axis(wTe, wTep)
+
+    if base.isscalar(gain):
+        k = gain * np.eye(6)
+    else:
+        k = np.diag(gain)
 
     v = k @ e
-
     arrived = True if np.sum(np.abs(e)) < threshold else False
 
     return v, arrived
