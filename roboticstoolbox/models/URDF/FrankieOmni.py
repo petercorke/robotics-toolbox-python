f07643f24d28e2c8c76c218d56d655c50fe2f41f
jhavl <jhavl@users.noreply.github.com>
2021-09-14 10:08:45 +1000

    Add onmidirectional mm

---
+++ roboticstoolbox/models/URDF/FrankieOmni.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import numpy as np
+from roboticstoolbox.robot.ERobot import ERobot
+from spatialmath import SE3
+
+
+class FrankieOmni(ERobot):
+    """
+    Class that imports an Omnidirectional Frankie URDF model
+
+    ``FrankieOmni()`` is a class which imports a FrankieOmni robot definition
+    from a URDF file.  The model describes its kinematic and graphical
+    characteristics.
+
+    .. runblock:: pycon
+
+        >>> import roboticstoolbox as rtb
+        >>> robot = rtb.models.URDF.FrankieOmni()
+        >>> print(robot)
+
+    Defined joint configurations are:
+
+    - qz, zero joint angle configuration, 'L' shaped configuration
+    - qr, vertical 'READY' configuration
+    - qs, arm is stretched out in the x-direction
+    - qn, arm is at a nominal non-singular configuration
+
+    .. codeauthor:: Jesse Haviland
+    .. sectionauthor:: Peter Corke
+    """
+
+    def __init__(self):
+
+        links, name, urdf_string, urdf_filepath = self.URDF_read(
+            "franka_description/robots/frankieOmni_arm_hand.urdf.xacro"
+        )
+
+        super().__init__(
+            links,
+            name=name,
+            manufacturer="Franka Emika",
+            gripper_links=links[12],
+            urdf_string=urdf_string,
+            urdf_filepath=urdf_filepath,
+        )
+
+        self.grippers[0].tool = SE3(0, 0, 0.1034)
+
+        self.qdlim = np.array(
+            [
+                4.0,
+                4.0,
+                4.0,
+                2.1750,
+                2.1750,
+                2.1750,
+                2.1750,
+                2.6100,
+                2.6100,
+                2.6100,
+                3.0,
+                3.0,
+            ]
+        )
+
+        self.addconfiguration("qz", np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+
+        self.addconfiguration(
+            "qr", np.array([0, 0, 0, 0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+        )
+
+
+if __name__ == "__main__":  # pragma nocover
+
+    robot = FrankieOmni()
+    print(robot)
+
+    for link in robot.links:
+        print(link.name)
+        print(link.isjoint)
+        print(len(link.collision))
+
+    print()
+
+    for link in robot.grippers[0].links:
+        print(link.name)
+        print(link.isjoint)
+        print(len(link.collision))
