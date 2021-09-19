9aafd4ab45d6e4e7a13e7407427ef75b6edfbd94
Peter Corke <peter.i.corke@gmail.com>
2021-09-20 08:03:12 +1000

    blocks now have one output q use RTB VehicleBase subclass objects use Vehicle animation class for graphics

--- roboticstoolbox/blocks/mobile.py
+++ roboticstoolbox/blocks/mobile.py
@@ -82,23 +82,14 @@ class Bicycle(TransferBlock):
             self._x0 = x0
             
         self.inport_names(('v', '$\gamma$'))
-        self.outport_names(('x', 'y', r'$\theta$'))
+        self.outport_names(('q',))
         self.state_names(('x', 'y', r'$\theta$'))
         
     def output(self, t):
         return [self._x]  # one output which is ndarray(3)
     
     def deriv(self):
-        theta = self._x[2]
-        
-        # get inputs and clip them
-        v = self.inputs[0]
-        v = min(self.vlim, max(v, -self.vlim))
-        gamma = self.inputs[1]
-        gamma = min(self.slim, max(gamma, -self.slim))
-        
-        xd = np.r_[v * cos(theta), v * sin(theta), v * tan(gamma)/self.L ]
-        return xd
+        return self.vehicle.deriv(self._x, self.inputs)
     
 # ------------------------------------------------------------------------ #
 class Unicycle(TransferBlock):
@@ -153,24 +144,36 @@ class Unicycle(TransferBlock):
 
         :seealso: :class:`Bicycle` :class:`DiffSteer`
         """        
-        super().__init__(inputs=inputs, **kwargs)
-        self.nstates = 3
+        super().__init__(nstates=3, **blockargs)
         
         if x0 is None:
-            self._x0 = np.zeros((slef.nstates,))
+            self._x0 = np.zeros((self.nstates,))
         else:
             assert len(x0) == self.nstates, "x0 is {:d} long, should be {:d}".format(len(x0), self.nstates)
             self._x0 = x0
+
+        self.vehicle = mobile.Unicycle(w=w,
+            steer_max=steer_max, speed_max=speed_max, accel_max=accel_max)
+
+        #TODO, add support for origin shift
+        #         If ``a`` is non-zero then the planar velocity of that point $x=a$
+        # can be controlled by
+
+        # .. math::
+
+        #     \begin{pmatrix} v \\ \omega \end{pmatrix} = 
+        #     \begin{pmatrix}
+        #         \cos \theta & \sin \theta \\ 
+        #         -\frac{1}{a}\sin \theta & \frac{1}{a}\cos \theta
+        #     \end{pmatrix}\begin{pmatrix}
+        #         \dot{x} \\ \dot{y}
+        #     \end{pmatrix}
         
     def output(self, t):
         return self._x
     
     def deriv(self):
-        theta = self._x[2]
-        v = self.inputs[0]
-        omega = self.inputs[1]
-        xd = np.r_[v * cos(theta), v * sin(theta), omega]
-        return xd
+        return self.vehicle.deriv(self._x, self.inputs)
     
 # ------------------------------------------------------------------------ #
 class DiffSteer(TransferBlock):
@@ -231,29 +234,28 @@ class DiffSteer(TransferBlock):
 
         :seealso: :class:`Bicycle` :class:`Unicycle`
         """
-        super().__init__(nin=2, nout=3, inputs=inputs, **kwargs)
-        self.nstates = 3
+        super().__init__(nstates=3, **blockargs)
         self.type = 'diffsteer'
         self.R = R
-        self.W = W
         
         if x0 is None:
             self._x0 = np.zeros((slef.nstates,))
         else:
             assert len(x0) == self.nstates, "x0 is {:d} long, should be {:d}".format(len(x0), self.nstates)
             self._x0 = x0
-        
+
+        self.vehicle = mobile.Unicycle(w=w,
+            steer_max=steer_max, speed_max=speed_max, accel_max=accel_max)
+
     def output(self, t):
         return self._x
     
     def deriv(self):
-        theta = self._x[2]
+        # compute (v, omega) from left/right wheel speeds
         v = self.R * (self.inputs[0] + self.inputs[1]) / 2
         omega = (self.inputs[1] + self.inputs[0]) / self.W
-    
-        xd = np.r_[v * cos(theta), v * sin(theta), omega]
-        return xd
-    
+        return self.vehicle.deriv(self._x, (v, omega))
+
 # ------------------------------------------------------------------------ #
 
 class VehiclePlot(GraphicsBlock):
@@ -318,7 +320,7 @@ class VehiclePlot(GraphicsBlock):
            Example of vehicle display (animated).  The label at the top is the
            block name.
         """
-        super().__init__(nin=3, inputs=inputs, **kwargs)
+        super().__init__(**blockargs)
         self.xdata = []
         self.ydata = []
         self.type = 'vehicleplot'
@@ -327,11 +329,7 @@ class VehiclePlot(GraphicsBlock):
         self.init = init
         self.square = square
 
-        self.path = path
-        if path:
-            self.pathstyle = pathstyle
-        self.color = color
-        self.fill = fill
+        self.pathstyle = path
         
         if scale != 'auto':
             if len(scale) == 2:
@@ -339,23 +337,15 @@ class VehiclePlot(GraphicsBlock):
         self.scale = scale
         self.labels = labels
         
-        d = size
-        if shape == 'triangle':
-            L = d
-            W = 0.6*d
-            vertices = [(L, 0), (-L, -W), (-L, W)]
-        elif shape == 'box':
-            L1 = d
-            L2 = d
-            W = 0.6*d
-            vertices = [(-L1, W), (0.6*L2, W), (L2, 0.5*W), (L2, -0.5*W), (0.6*L2, -W), (-L1, -W)]
-        else:
-            raise ValueError('bad vehicle shape specified')
-        self.vertices_hom = base.e2h(np.array(vertices).T)
-        self.vertices = np.array(vertices)
+        if animation is None:
+            animation = mobile.VehiclePolygon()
+        elif not isinstance(animation, mobile.VehicleAnimationBase):
+            raise TypeError('animation object must be VehicleAnimationBase subclass')
+
+        self.animation = animation
 
         
-    def start(self, state=None, **kwargs):
+    def start(self, state=None):
         # create the plot
         super().reset()
         try:
@@ -370,18 +360,6 @@ class VehiclePlot(GraphicsBlock):
             self.ax.set_aspect('equal')
         print('done')
 
-        args = []
-        kwargs = {}
-        if self.path:
-            style = self.pathstyle
-            if isinstance(style, dict):
-                kwargs = style
-            elif isinstance(style, str):
-                args = [style]
-            self.line, = self.ax.plot(self.xdata, self.ydata, *args, **kwargs)
-        poly = Polygon(self.vertices, closed=True, edgecolor=self.color, facecolor=self.fill)
-        self.vehicle = self.ax.add_patch(poly)
-
         self.ax.grid(True)
         self.ax.set_xlabel(self.labels[0])
         self.ax.set_ylabel(self.labels[1])
@@ -391,6 +369,13 @@ class VehiclePlot(GraphicsBlock):
             self.ax.set_ylim(*self.scale[2:4])
         if self.init is not None:
             self.init(self.ax)
+
+        if isinstance(self.pathstyle, str):
+            self.line, = plt.plot(0, 0, self.pathstyle)
+        elif isinstance(self.pathstyle, dict):
+            self.line, = plt.plot(0, 0, **self.pathstyle)
+
+        self.animation.add()
             
         plt.draw()
         plt.show(block=False)
@@ -399,14 +384,17 @@ class VehiclePlot(GraphicsBlock):
         
     def step(self, state=None, **kwargs):
         # inputs are set
-        self.xdata.append(self.inputs[0])
-        self.ydata.append(self.inputs[1])
+        xyt = self.inputs[0]
+
+        # update the path line
+        self.xdata.append(xyt[0])
+        self.ydata.append(xyt[1])
         #plt.figure(self.fig.number)
-        if self.path:
+        if self.pathstyle is not None:
             self.line.set_data(self.xdata, self.ydata)
-        T = base.transl2(self.inputs[0], self.inputs[1]) @ base.trot2(self.inputs[2])
-        new = base.h2e(T @ self.vertices_hom)
-        self.vehicle.set_xy(new.T)
+
+        # update the vehicle pose
+        self.animation.update(xyt)
     
         if self.scale == 'auto':
             self.ax.relim()
