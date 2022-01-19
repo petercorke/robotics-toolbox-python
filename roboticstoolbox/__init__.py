e7f0ca437a960fccb79787ce533c0b48fb6028a7
jhavl <jhavl@users.noreply.github.com>
2022-01-20 06:51:04 +1000

    fixed imports

--- roboticstoolbox/__init__.py
+++ roboticstoolbox/__init__.py
@@ -3,5 +3,7 @@ from roboticstoolbox.tools import *
 from roboticstoolbox.robot import *
 
 from roboticstoolbox.mobile import *
-import roboticstoolbox.models
-import roboticstoolbox.backends
+from roboticstoolbox import models
+from roboticstoolbox import backends
+
+__all__ = ["models", "backends"]
