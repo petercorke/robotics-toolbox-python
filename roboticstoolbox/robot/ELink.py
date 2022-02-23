84bf48b14bf333e488f0c7eee7667f99878ea37a
jhavl <jhavl@users.noreply.github.com>
2022-02-24 09:29:12 +1000

    added *args

--- roboticstoolbox/robot/ELink.py
+++ roboticstoolbox/robot/ELink.py
@@ -8,16 +8,16 @@ from roboticstoolbox.robot.Link import Link, Link2
 
 
 class ELink(Link):
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         import warnings
 
         warnings.warn("ELink is deprecated, use Link instead", FutureWarning)
-        super().__init__(**kwargs)
+        super().__init__(*args, **kwargs)
 
 
 class ELink2(Link2):
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         import warnings
 
         warnings.warn("ELink2 is deprecated, use Link2 instead", FutureWarning)
-        super().__init__(**kwargs)
+        super().__init__(*args, **kwargs)
