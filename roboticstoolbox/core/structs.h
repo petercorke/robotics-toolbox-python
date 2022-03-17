50259179c5f8dfa07383ed544099d235d34ccce1
jhavl <jhavl@users.noreply.github.com>
2022-03-17 16:19:00 +1000

    added Tm

--- roboticstoolbox/core/structs.h
+++ roboticstoolbox/core/structs.h
@@ -9,9 +9,9 @@
 #define STRUCTS_H
 
 // #include "linalg.h"
-// #ifdef __cplusplus
-// #include <Eigen/Dense>
-// #endif /* __cplusplus */
+#ifdef __cplusplus
+#include <Eigen/Dense>
+#endif /* __cplusplus */
 
 #ifdef __cplusplus
 extern "C"
@@ -31,10 +31,13 @@ extern "C"
         int isflip;
         int jindex;
         int axis;
-        double *T; /* link static transform */
-        // Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> Tm;
+        double *T;    /* link static transform */
         double *qlim; /* joint limits */
         void (*op)(double *data, double eta);
+
+#ifdef __cplusplus
+        Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> Tm;
+#endif /* __cplusplus */
     };
 
     // struct ET
