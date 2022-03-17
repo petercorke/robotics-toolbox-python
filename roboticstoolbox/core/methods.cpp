115b819fab85ae3c8aa076bc9f78780e9c2892c9
jhavl <jhavl@users.noreply.github.com>
2022-03-17 16:01:18 +1000

    extracted methods from fknm

---
+++ roboticstoolbox/core/methods.cpp
@@ -0,0 +1,433 @@
+/* methods.cpp */
+
+#include "linalg.h"
+#include "methods.h"
+#include "structs.h"
+
+#include <Python.h>
+#include <math.h>
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/QR>
+
+extern "C"
+{
+    // Forward defines
+    // void _ETS_IK(PyObject *ets, int n, double *q, double *Tep, double *ret);
+    // void _ETS_hessian(int n, double *J, double *H);
+    // void _ETS_jacob0(PyObject *ets, int n, double *q, double *tool, double *J);
+    // void _ETS_jacobe(PyObject *ets, int n, double *q, double *tool, double *J);
+    // void _ETS_fkine(PyObject *ets, double *q, double *base, double *tool, double *ret);
+    // void _ET_T(ET *et, double *ret, double eta);
+    // void _ET_Alloc(ET *et);
+
+    // Methods
+
+    void _ETS_IK(PyObject *ets, int n, double *q, double *Tep, double *ret)
+    {
+        // double E;
+        double *Te = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *e = (double *)PyMem_RawCalloc(6, sizeof(double));
+
+        double *a = (double *)PyMem_RawCalloc(6, sizeof(double));
+        // a[0] = 1.0;
+        // a[1] = 4.0;
+        // a[2] = 2.0;
+        // a[3] = 5.0;
+        // a[4] = 3.0;
+        // a[5] = 6.0;
+        a[0] = 1.0;
+        a[1] = 2.0;
+        a[2] = 3.0;
+        a[3] = 4.0;
+        a[4] = 5.0;
+        a[5] = 6.0;
+        double *b = (double *)PyMem_RawCalloc(12, sizeof(double));
+        b[0] = 11.0;
+        b[1] = 15.0;
+        b[2] = 19.0;
+        b[3] = 12.0;
+        b[4] = 16.0;
+        b[5] = 20.0;
+        b[6] = 13.0;
+        b[7] = 17.0;
+        b[8] = 21.0;
+        b[9] = 14.0;
+        b[10] = 18.0;
+        b[11] = 22.0;
+        // b[0] = 11.0;
+        // b[1] = 12.0;
+        // b[2] = 13.0;
+        // b[3] = 14.0;
+        // b[4] = 15.0;
+        // b[5] = 16.0;
+        // b[6] = 17.0;
+        // b[7] = 18.0;
+        // b[8] = 19.0;
+        // b[9] = 20.0;
+        // b[10] = 21.0;
+        // b[11] = 22.0;
+
+        // double *U = (double *)PyMem_RawCalloc(16, sizeof(double));
+        // double *invU = (double *)PyMem_RawCalloc(16, sizeof(double));
+        // double *temp = (double *)PyMem_RawCalloc(16, sizeof(double));
+        // double *ret = (double *)PyMem_RawCalloc(16, sizeof(double));
+        // Py_ssize_t m;
+        int arrived = 0, iter = 0;
+
+        while (arrived == 0 && iter < 500)
+        {
+            // Current pose Te
+            _ETS_fkine(ets, q, (double *)NULL, NULL, Te);
+
+            // Angle axis error e
+            _angle_axis(Te, Tep, e);
+
+            // Squared error E
+            // E = 0.5 * e @ We @ e
+            // E = 0.5 * (e[0] * e[0] + e[1] * e[1] + e[2] * e[2] + e[3] * e[3] + e[4] * e[4] + e[5] * e[5]);
+        }
+
+        // _ETS_fkine(ets, q, (double *)NULL, NULL, Te);
+
+        // for (int i = 0; i < 2; i++)
+        // {
+        //     for (int j = 0; j < 4; j++)
+        //     {
+        //         ret[i * 4 + j] = 0.0;
+        //     }
+        // }
+
+        // _mult_T(2, 3, 0, a, 3, 4, 0, b, ret);
+        // _mult_T(3, 2, 1, a, 3, 4, 0, b, ret);
+        // _mult_T(3, 2, 1, a, 4, 3, 1, b, ret);
+        // _mult_T(2, 3, 0, a, 4, 3, 1, b, ret);
+
+        // int j = 0;
+    }
+
+    void _ETS_hessian(int n, double *J, double *H)
+    {
+        int a, b;
+        int n2 = 2 * n, n3 = 3 * n, n4 = 4 * n, n5 = 5 * n;
+
+        for (int j = 0; j < n; j++)
+        {
+            a = j * 6 * n;
+            for (int i = j; i < n; i++)
+            {
+                b = i * 6 * n;
+                _cross(J + j + n3, J + i, H + a + i, n);
+                _cross(J + j + n3, J + i + n3, H + a + i + n3, n);
+
+                if (i != j)
+                {
+                    H[b + j] = H[a + i];
+                    H[b + j + n] = H[a + i + n];
+                    H[b + j + n2] = H[a + i + n2];
+                    H[b + j + n3] = 0;
+                    H[b + j + n4] = 0;
+                    H[b + j + n5] = 0;
+                }
+            }
+        }
+    }
+
+    void _ETS_jacob0(PyObject *ets, int n, double *q, double *tool, double *J)
+    {
+        ET *et;
+        double *T = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *U = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *invU = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *temp = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *ret = (double *)PyMem_RawCalloc(16, sizeof(double));
+        Py_ssize_t m;
+
+        int j = 0;
+
+        _eye4(U);
+
+        // Get the forward  kinematics into T
+        _ETS_fkine(ets, q, (double *)NULL, tool, T);
+
+        PyObject *iter_et = PyObject_GetIter(ets);
+
+        m = PyList_GET_SIZE(ets);
+        for (int i = 0; i < m; i++)
+        {
+            if (!(et = (ET *)PyCapsule_GetPointer(PyIter_Next(iter_et), "ET")))
+                return;
+
+            if (et->isjoint)
+            {
+                _ET_T(et, ret, q[et->jindex]);
+                _mult4(U, ret, temp);
+                // cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 4, 4, 4, 1.0, U, 4, ret, 4, 0, temp, 4);
+                _copy(temp, U);
+
+                if (i == m - 1 && tool != NULL)
+                {
+                    _mult4(U, tool, temp);
+                    // cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 4, 4, 4, 1.0, U, 4, tool, 4, 0, temp, 4);
+                    _copy(temp, U);
+                }
+
+                _inv(U, invU);
+                _mult4(invU, T, temp);
+                // cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 4, 4, 4, 1.0, invU, 4, T, 4, 0, temp, 4);
+
+                if (et->axis == 0)
+                {
+                    J[0 * n + j] = U[0 * 4 + 2] * temp[1 * 4 + 3] - U[0 * 4 + 1] * temp[2 * 4 + 3];
+                    J[1 * n + j] = U[1 * 4 + 2] * temp[1 * 4 + 3] - U[1 * 4 + 1] * temp[2 * 4 + 3];
+                    J[2 * n + j] = U[2 * 4 + 2] * temp[1 * 4 + 3] - U[2 * 4 + 1] * temp[2 * 4 + 3];
+                    J[3 * n + j] = U[0 * 4 + 0];
+                    J[4 * n + j] = U[1 * 4 + 0];
+                    J[5 * n + j] = U[2 * 4 + 0];
+                }
+                else if (et->axis == 1)
+                {
+                    J[0 * n + j] = U[0 * 4 + 0] * temp[2 * 4 + 3] - U[0 * 4 + 2] * temp[0 * 4 + 3];
+                    J[1 * n + j] = U[1 * 4 + 0] * temp[2 * 4 + 3] - U[1 * 4 + 2] * temp[0 * 4 + 3];
+                    J[2 * n + j] = U[2 * 4 + 0] * temp[2 * 4 + 3] - U[2 * 4 + 2] * temp[0 * 4 + 3];
+                    J[3 * n + j] = U[0 * 4 + 1];
+                    J[4 * n + j] = U[1 * 4 + 1];
+                    J[5 * n + j] = U[2 * 4 + 1];
+                }
+                else if (et->axis == 2)
+                {
+                    J[0 * n + j] = U[0 * 4 + 1] * temp[0 * 4 + 3] - U[0 * 4 + 0] * temp[1 * 4 + 3];
+                    J[1 * n + j] = U[1 * 4 + 1] * temp[0 * 4 + 3] - U[1 * 4 + 0] * temp[1 * 4 + 3];
+                    J[2 * n + j] = U[2 * 4 + 1] * temp[0 * 4 + 3] - U[2 * 4 + 0] * temp[1 * 4 + 3];
+                    J[3 * n + j] = U[0 * 4 + 2];
+                    J[4 * n + j] = U[1 * 4 + 2];
+                    J[5 * n + j] = U[2 * 4 + 2];
+                }
+                else if (et->axis == 3)
+                {
+                    J[0 * n + j] = U[0 * 4 + 0];
+                    J[1 * n + j] = U[1 * 4 + 0];
+                    J[2 * n + j] = U[2 * 4 + 0];
+                    J[3 * n + j] = 0.0;
+                    J[4 * n + j] = 0.0;
+                    J[5 * n + j] = 0.0;
+                }
+                else if (et->axis == 4)
+                {
+                    J[0 * n + j] = U[0 * 4 + 1];
+                    J[1 * n + j] = U[1 * 4 + 1];
+                    J[2 * n + j] = U[2 * 4 + 1];
+                    J[3 * n + j] = 0.0;
+                    J[4 * n + j] = 0.0;
+                    J[5 * n + j] = 0.0;
+                }
+                else if (et->axis == 5)
+                {
+                    J[0 * n + j] = U[0 * 4 + 2];
+                    J[1 * n + j] = U[1 * 4 + 2];
+                    J[2 * n + j] = U[2 * 4 + 2];
+                    J[3 * n + j] = 0.0;
+                    J[4 * n + j] = 0.0;
+                    J[5 * n + j] = 0.0;
+                }
+                j++;
+            }
+            else
+            {
+                _ET_T(et, ret, q[et->jindex]);
+                _mult4(U, ret, temp);
+                // cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 4, 4, 4, 1.0, U, 4, ret, 4, 0, temp, 4);
+                _copy(temp, U);
+            }
+        }
+
+        Py_DECREF(iter_et);
+
+        free(T);
+        free(U);
+        free(temp);
+        free(ret);
+        free(invU);
+    }
+
+    void _ETS_jacobe(PyObject *ets, int n, double *q, double *tool, double *J)
+    {
+        ET *et;
+        double *T = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *U = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *temp = (double *)PyMem_RawCalloc(16, sizeof(double));
+        double *ret = (double *)PyMem_RawCalloc(16, sizeof(double));
+        Py_ssize_t m;
+
+        int j = n - 1;
+
+        _eye4(U);
+
+        // Get the forward  kinematics into T
+        _ETS_fkine(ets, q, (double *)NULL, tool, T);
+
+        PyList_Reverse(ets);
+        PyObject *iter_et = PyObject_GetIter(ets);
+
+        if (tool != NULL)
+        {
+            _mult4(tool, U, temp);
+            _copy(temp, U);
+        }
+
+        m = PyList_GET_SIZE(ets);
+        for (int i = 0; i < m; i++)
+        {
+            if (!(et = (ET *)PyCapsule_GetPointer(PyIter_Next(iter_et), "ET")))
+                return;
+
+            if (et->isjoint)
+            {
+                if (et->axis == 0)
+                {
+                    J[0 * n + j] = U[2 * 4 + 0] * U[1 * 4 + 3] - U[1 * 4 + 0] * U[2 * 4 + 3];
+                    J[1 * n + j] = U[2 * 4 + 1] * U[1 * 4 + 3] - U[1 * 4 + 1] * U[2 * 4 + 3];
+                    J[2 * n + j] = U[2 * 4 + 2] * U[1 * 4 + 3] - U[1 * 4 + 2] * U[2 * 4 + 3];
+                    J[3 * n + j] = U[0 * 4 + 0];
+                    J[4 * n + j] = U[0 * 4 + 1];
+                    J[5 * n + j] = U[0 * 4 + 2];
+                }
+                else if (et->axis == 1)
+                {
+                    J[0 * n + j] = U[0 * 4 + 0] * U[2 * 4 + 3] - U[2 * 4 + 0] * U[0 * 4 + 3];
+                    J[1 * n + j] = U[0 * 4 + 1] * U[2 * 4 + 3] - U[2 * 4 + 1] * U[0 * 4 + 3];
+                    J[2 * n + j] = U[0 * 4 + 2] * U[2 * 4 + 3] - U[2 * 4 + 2] * U[0 * 4 + 3];
+                    J[3 * n + j] = U[1 * 4 + 0];
+                    J[4 * n + j] = U[1 * 4 + 1];
+                    J[5 * n + j] = U[1 * 4 + 2];
+                }
+                else if (et->axis == 2)
+                {
+                    J[0 * n + j] = U[1 * 4 + 0] * U[0 * 4 + 3] - U[0 * 4 + 0] * U[1 * 4 + 3];
+                    J[1 * n + j] = U[1 * 4 + 1] * U[0 * 4 + 3] - U[0 * 4 + 1] * U[1 * 4 + 3];
+                    J[2 * n + j] = U[1 * 4 + 2] * U[0 * 4 + 3] - U[0 * 4 + 2] * U[1 * 4 + 3];
+                    J[3 * n + j] = U[2 * 4 + 0];
+                    J[4 * n + j] = U[2 * 4 + 1];
+                    J[5 * n + j] = U[2 * 4 + 2];
+                }
+                else if (et->axis == 3)
+                {
+                    J[0 * n + j] = U[0 * 4 + 0];
+                    J[1 * n + j] = U[0 * 4 + 1];
+                    J[2 * n + j] = U[0 * 4 + 2];
+                    J[3 * n + j] = 0.0;
+                    J[4 * n + j] = 0.0;
+                    J[5 * n + j] = 0.0;
+                }
+                else if (et->axis == 4)
+                {
+                    J[0 * n + j] = U[1 * 4 + 0];
+                    J[1 * n + j] = U[1 * 4 + 1];
+                    J[2 * n + j] = U[1 * 4 + 2];
+                    J[3 * n + j] = 0.0;
+                    J[4 * n + j] = 0.0;
+                    J[5 * n + j] = 0.0;
+                }
+                else if (et->axis == 5)
+                {
+                    J[0 * n + j] = U[2 * 4 + 0];
+                    J[1 * n + j] = U[2 * 4 + 1];
+                    J[2 * n + j] = U[2 * 4 + 2];
+                    J[3 * n + j] = 0.0;
+                    J[4 * n + j] = 0.0;
+                    J[5 * n + j] = 0.0;
+                }
+
+                _ET_T(et, ret, q[et->jindex]);
+                _mult4(ret, U, temp);
+                _copy(temp, U);
+                j--;
+            }
+            else
+            {
+                _ET_T(et, ret, q[et->jindex]);
+                _mult4(ret, U, temp);
+                _copy(temp, U);
+            }
+        }
+
+        PyList_Reverse(ets);
+
+        Py_DECREF(iter_et);
+
+        free(T);
+        free(U);
+        free(temp);
+        free(ret);
+    }
+
+    void _ETS_fkine(PyObject *ets, double *q, double *base, double *tool, double *ret)
+    {
+        double *temp, *current;
+        ET *et;
+        Py_ssize_t m;
+
+        temp = (double *)PyMem_RawCalloc(16, sizeof(double));
+        current = (double *)PyMem_RawCalloc(16, sizeof(double));
+        PyObject *iter_et = PyObject_GetIter(ets);
+
+        if (base != NULL)
+        {
+            _copy(base, current);
+        }
+        else
+        {
+            _eye4(current);
+        }
+
+        m = PyList_GET_SIZE(ets);
+        for (int i = 0; i < m; i++)
+        {
+            if (!(et = (ET *)PyCapsule_GetPointer(PyIter_Next(iter_et), "ET")))
+                return;
+
+            _ET_T(et, ret, q[et->jindex]);
+            _mult4(current, ret, temp);
+            _copy(temp, current);
+        }
+
+        if (tool != NULL)
+        {
+            _mult4(current, tool, ret);
+        }
+        else
+        {
+            _copy(current, ret);
+        }
+
+        Py_DECREF(iter_et);
+
+        free(temp);
+        free(current);
+    }
+
+    void _ET_T(ET *et, double *ret, double eta)
+    {
+        // Check if static and return static transform
+        if (!et->isjoint)
+        {
+            _copy(et->T, ret);
+            return;
+        }
+
+        if (et->isflip)
+        {
+            eta = -eta;
+        }
+
+        // Calculate ET trasform based on eta
+        et->op(ret, eta);
+    }
+
+    void _ET_Alloc(ET *et)
+    {
+        // new (&et->Tm) Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>>(et->T);
+        // Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> et->Tm(et->T);
+    }
+
+} /* extern "C" */
---
+++ roboticstoolbox/core/methods.h
@@ -0,0 +1,77 @@
+/**
+ * \file methods.h
+ * \author Jesse Haviland
+ *
+ */
+/* methods.h */
+
+// #include "linalg.h"
+// #include <Eigen/Dense>
+
+#include <Python.h>
+#include "structs.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+    void _ETS_IK(PyObject *ets, int n, double *q, double *Tep, double *ret);
+    void _ETS_hessian(int n, double *J, double *H);
+    void _ETS_jacob0(PyObject *ets, int n, double *q, double *tool, double *J);
+    void _ETS_jacobe(PyObject *ets, int n, double *q, double *tool, double *J);
+    void _ETS_fkine(PyObject *ets, double *q, double *base, double *tool, double *ret);
+    void _ET_T(ET *et, double *ret, double eta);
+    void _ET_Alloc(ET *et);
+
+    // #include "structs.h"
+
+    // typedef struct ET ET;
+    // typedef struct Link Link;
+
+    // struct ET
+    // {
+    //     /**********************************************************
+    //      *************** kinematic parameters *********************
+    //      **********************************************************/
+    //     int isjoint;
+    //     int isflip;
+    //     int jindex;
+    //     int axis;
+    //     double *T; /* link static transform */
+    //     // Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> Tm;
+    //     double *qlim; /* joint limits */
+    //     void (*op)(double *data, double eta);
+    // };
+
+    // struct Link
+    // {
+    //     /**********************************************************
+    //      *************** kinematic parameters *********************
+    //      **********************************************************/
+    //     int isjoint;
+    //     int isflip;
+    //     int jindex;
+    //     int axis;
+    //     int n_shapes;
+    //     double *A;  /* link static transform */
+    //     double *fk; /* link world transform */
+    //     void (*op)(double *data, double eta);
+    //     Link *parent;
+    //     double **shape_base; /* link visual and collision geometries */
+    //     double **shape_wT;   /* link visual and collision geometries */
+    //     double **shape_sT;   /* link visual and collision geometries */
+    //     double **shape_sq;   /* link visual and collision geometries */
+    // };
+
+    // int _check_array_type(PyObject *toCheck);
+    // void _ETS_IK(PyObject *ets, int n, double *q, double *Tep, double *ret);
+    // void _ETS_hessian(int n, double *J, double *H);
+    // void _ETS_jacob0(PyObject *ets, int n, double *q, double *tool, double *J);
+    // void _ETS_jacobe(PyObject *ets, int n, double *q, double *tool, double *J);
+    // void _ETS_fkine(PyObject *ets, double *q, double *base, double *tool, double *ret);
+    // void _ET_T(ET *et, double *ret, double eta);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
