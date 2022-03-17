19bb0aae4e800ca86bdf3ffec31c2a1f2d306a22
jhavl <jhavl@users.noreply.github.com>
2022-03-17 16:02:04 +1000

    added struct file

---
+++ roboticstoolbox/core/structs.h
@@ -0,0 +1,59 @@
+/**
+ * \file structs.h
+ * \author Jesse Haviland
+ *
+ */
+/* structs.h */
+
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+// #include "linalg.h"
+// #ifdef __cplusplus
+// #include <Eigen/Dense>
+// #endif /* __cplusplus */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+    // #include <Eigen/Dense>
+
+    typedef struct ET ET;
+
+    struct ET
+    {
+        /**********************************************************
+         *************** kinematic parameters *********************
+         **********************************************************/
+        int isjoint;
+        int isflip;
+        int jindex;
+        int axis;
+        double *T; /* link static transform */
+        // Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> Tm;
+        double *qlim; /* joint limits */
+        void (*op)(double *data, double eta);
+    };
+
+    // struct ET
+    // {
+    //     /**********************************************************
+    //      *************** kinematic parameters *********************
+    //      **********************************************************/
+    //     int isjoint;
+    //     int isflip;
+    //     int jindex;
+    //     int axis;
+    //     double *T; /* link static transform */
+    //     // Eigen::Map<Eigen::Matrix<double, 4, 4, Eigen::RowMajor>> Tm;
+    //     double *qlim; /* joint limits */
+    //     void (*op)(double *data, double eta);
+    // };
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif
