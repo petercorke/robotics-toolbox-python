8b670204ef29618019fb1edbe0c218ddd6ce6562
Jens Kober <jkober@tudelft.nl>
2022-07-13 18:00:37 +0200

    restored empty lines that got removed

--- roboticstoolbox/models/DH/Panda.py
+++ roboticstoolbox/models/DH/Panda.py
@@ -8,18 +8,24 @@ from roboticstoolbox import DHRobot, RevoluteMDH
 class Panda(DHRobot):
     """
     A class representing the Panda robot arm.
+
     ``Panda()`` is a class which models a Franka-Emika Panda robot and
     describes its kinematic characteristics using modified DH
     conventions.
+
     .. runblock:: pycon
+
         >>> import roboticstoolbox as rtb
         >>> robot = rtb.models.DH.Panda()
         >>> print(robot)
+
     .. note::
         - SI units of metres are used.
         - The model includes a tool offset.
+
     :references:
         - https://frankaemika.github.io/docs/control_parameters.html
+
     .. codeauthor:: Samuel Drew
     .. codeauthor:: Peter Corke
     """
