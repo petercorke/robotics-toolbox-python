a7aa3ff479148fe0f61b82660f2d7442f48e94b5
jhavl <jhavl@users.noreply.github.com>
2022-09-28 08:40:56 +1000

    added arraylike type

---
+++ roboticstoolbox/tools/types.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+@author Jesse Haviland
+"""
+
+from typing import Tuple, Union, List, Tuple, Set
+from numpy import ndarray
+
+ArrayLike = Union[ndarray, List, Tuple, Set]
