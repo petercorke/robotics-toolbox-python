21b53636ae8549feb4f401e71d586c40775b1457
jhavl <jhavl@users.noreply.github.com>
2022-09-29 18:02:35 +1000

    add IK_NR tests

---
+++ tests/test_IK.py
@@ -0,0 +1,180 @@
+"""
+@author: Jesse Haviland
+"""
+
+import numpy.testing as nt
+import roboticstoolbox as rtb
+import numpy as np
+
+# import spatialmath.base as sm
+from spatialmath import SE3
+from spatialmath.base import tr2jac
+import unittest
+import sympy
+
+
+class TestIK(unittest.TestCase):
+    def test_IK_NR1(self):
+
+        tol = 1e-6
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.eval([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+
+        solver = rtb.IK_NR(joint_limits=True, seed=0, pinv=True, tol=tol)
+
+        sol = solver.solve(panda, Tep)
+
+        self.assertEqual(sol.success, True)
+
+        Tq = panda.eval(sol.q)
+
+        _, E = solver.error(Tep, Tq)
+
+        self.assertGreater(tol, E)
+
+    def test_IK_NR2(self):
+
+        q0 = [1, 2, 3, 4, 5, 6, 7]
+
+        tol = 1e-6
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.eval([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+
+        solver = rtb.IK_NR(joint_limits=True, seed=0, pinv=True, tol=tol)
+
+        sol = solver.solve(panda, Tep, q0=q0)
+
+        self.assertEqual(sol.success, True)
+
+        Tq = panda.eval(sol.q)
+
+        _, E = solver.error(Tep, Tq)
+
+        self.assertGreater(tol, E)
+
+    def test_IK_NR3(self):
+
+        q0 = np.array([1, 2, 3, 4, 5, 6, 7])
+
+        tol = 1e-6
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.eval([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+
+        solver = rtb.IK_NR(joint_limits=True, seed=0, pinv=True, tol=tol)
+
+        sol = solver.solve(panda, Tep, q0=q0)
+
+        self.assertEqual(sol.success, True)
+
+        Tq = panda.eval(sol.q)
+
+        _, E = solver.error(Tep, Tq)
+
+        self.assertGreater(tol, E)
+
+    def test_IK_NR4(self):
+
+        q0 = np.array(
+            [
+                [1.0, 2, 3, 4, 5, 6, 7],
+                [1.0, 2, 1, 2, 1, 2, 1],
+                [1.0, 2, 3, 1, 2, 3, 1],
+                [2.0, 2, 2, 2, 2, 2, 2],
+                [0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4],
+            ]
+        )
+
+        tol = 1e-6
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.eval([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+
+        solver = rtb.IK_NR(joint_limits=True, seed=0, pinv=True, tol=tol, slimit=5)
+
+        sol = solver.solve(panda, Tep, q0=q0)
+
+        self.assertEqual(sol.success, True)
+
+        Tq = panda.eval(sol.q)
+
+        _, E = solver.error(Tep, Tq)
+
+        self.assertGreater(tol, E)
+
+    def test_IK_NR5(self):
+
+        tol = 1e-6
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.fkine([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+
+        solver = rtb.IK_NR(joint_limits=True, seed=0, pinv=True, tol=tol)
+
+        sol = solver.solve(panda, Tep)
+
+        self.assertEqual(sol.success, True)
+
+        Tq = panda.eval(sol.q)
+
+        _, E = solver.error(Tep.A, Tq)
+
+        self.assertGreater(tol, E)
+
+    def test_IK_NR6(self):
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.eval([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+
+        solver = rtb.IK_NR(joint_limits=True, seed=0, pinv=False, slimit=1)
+
+        sol = solver.solve(panda, Tep)
+
+        self.assertEqual(sol.success, False)
+
+    def test_IK_NR7(self):
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.eval([0, -1.3, 0, 1.2, 0, 2.0, 0.1])
+
+        solver = rtb.IK_NR(joint_limits=True, seed=0, pinv=True, slimit=2)
+
+        sol = solver.solve(panda, Tep)
+
+        self.assertEqual(sol.success, False)
+
+    def test_IK_NR8(self):
+
+        tol = 1e-6
+
+        panda = rtb.models.Panda().ets()
+
+        Tep = panda.eval([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
+
+        solver = rtb.IK_NR(
+            joint_limits=True, seed=0, pinv=True, tol=tol, kq=1.0, km=1.0
+        )
+
+        sol = solver.solve(panda, Tep)
+
+        self.assertEqual(sol.success, True)
+
+        Tq = panda.eval(sol.q)
+
+        _, E = solver.error(Tep, Tq)
+
+        self.assertGreater(tol, E)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
