56da64738b3da4fda56859f8a0747c5c4e1a6a01
Peter Corke <peter.i.corke@gmail.com>
2023-01-24 21:12:52 +1000

    fix bug in blocks URL

--- roboticstoolbox/blocks/__init__.py
+++ roboticstoolbox/blocks/__init__.py
@@ -3,4 +3,4 @@ from .mobile import *
 from .uav import *
 from .spatial import *
 
-url = "https://petercorke.github.io/bdsim/" + __package__
+url = "https://petercorke.github.io/robotics-toolbox-python/" + __package__
