b1cadc85df914b0ab0bc5b429e98c5db81444654
hucik14 <hucik14@gmail.com>
2023-05-02 09:21:59 +0200

    conflicts from future cleaning

--- roboticstoolbox/robot/PoERobot.py
+++ roboticstoolbox/robot/PoERobot.py
@@ -39,18 +39,6 @@ class PoELink(Link):
         s += "]"
         return s
 
-    def _repr_pretty_(self, p, cycle):
-        """
-        Pretty string for IPython (superclass method)
-
-        :param p: pretty printer handle (ignored)
-        :param cycle: pretty printer flag (ignored)
-
-        """
-        # see https://ipython.org/ipython-doc/stable/api/generated/IPython.lib.pretty.html
-
-        p.text(f"{i}:\n{str(x)}")
-
     def _ets_world(self, twist: Twist3) -> ETS:
         """
         Convert twist to its corresponding ETS
@@ -201,26 +189,13 @@ class PoERobot(Robot):
         return s
 
     def __repr__(self):
-        s = "PoERobot([\n    "
-        for j, link in enumerate(self):
-            s += repr(link) + ","
-        s += "],\n"
+        s = "PoERobot([\n"
+        s += "\n".join(["    " + repr(link) + "," for link in self])
+        s += "\n    ],\n"
         s += f"    T0=SE3({np.array_repr(self.T0.A)}),\n"
-        s += f"    name={self.name},\n"
+        s += f"    name=\"{self.name}\",\n"
         s += ")"
         return s
-
-    def _repr_pretty_(self, p, cycle):
-        """
-        Pretty string for IPython (superclass method)
-
-        :param p: pretty printer handle (ignored)
-        :param cycle: pretty printer flag (ignored)
-
-        """
-        # see https://ipython.org/ipython-doc/stable/api/generated/IPython.lib.pretty.html
-
-        p.text(f"{i}:\n{str(x)}")
 
     def nbranches(self):
         return 0
