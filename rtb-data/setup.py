3fd199a5037c7d05017137de85a5965af19240ea
Jesse Haviland <jhavl@users.noreply.github.com>
2023-05-15 12:26:13 +1000

    Update setup.py

--- rtb-data/setup.py
+++ rtb-data/setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-release = "1.0.0"
+release = "1.1.0"
 
 # list all data folders here, to ensure they get packaged
 
