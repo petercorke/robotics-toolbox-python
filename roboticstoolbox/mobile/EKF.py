7d9d48e114add2ccee803ddbc15472df1dc31cf1
Theo Combelles <theo.combelles@gmail.com>
2024-03-10 21:15:48 +0900

    The randn import has been changed from scipy to numpy.random

--- roboticstoolbox/mobile/EKF.py
+++ roboticstoolbox/mobile/EKF.py
@@ -6,7 +6,8 @@ Python EKF Planner
 from collections import namedtuple
 import numpy as np
 from math import pi
-from scipy import integrate, randn
+from scipy import integrate
+from numpy.random import randn
 from scipy.linalg import sqrtm, block_diag
 from scipy.stats.distributions import chi2
 import matplotlib.pyplot as plt
