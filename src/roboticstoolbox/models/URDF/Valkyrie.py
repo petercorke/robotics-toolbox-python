f88d22f4a179e1adc71bbea7c6060c525a848b2f
Peter Corke <peter.i.corke@gmail.com>
2026-09-15 12:20:19 +0200

    docs(models): credit robot_descriptions in the 8 model classes that use it
    Fetch, Frankie, Jaco, PR2, UR10, UR3, UR5, Valkyrie and YuMi all load
    their URDF via the robot_descriptions package under the hood, but none
    of their docstrings said so -- a reader had no way to know these models
    come from a third-party package. Reuses the same URL already used as
    URDFRobot.py's _RD_URL constant so the two don't drift.
    
    Verified: all 8 classes still import cleanly and the new sentence
    appears in each docstring.
    
    Addresses one item of #573

--- src/roboticstoolbox/models/URDF/Valkyrie.py
+++ src/roboticstoolbox/models/URDF/Valkyrie.py
@@ -41,6 +41,9 @@ class Valkyrie(Robot):
     from a URDF file.  The model describes its kinematic and graphical
     characteristics.
 
+    The model is loaded via the `robot_descriptions
+    <https://github.com/robot-descriptions/robot_descriptions.py>`_ package.
+
     .. runblock:: pycon
 
         >>> import roboticstoolbox as rtb
